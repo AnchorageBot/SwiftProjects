a4b6129065e56b40db9bff8ab10264da36014330
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-03-27 11:13:07 -0800

    Create HelloWorldApp.swift

---
+++ HelloWorldApp.swift
@@ -0,0 +1,21 @@
+/
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by ATS on 3/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
