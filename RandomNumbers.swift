561a382014ac75c4a061e49ebde3a9bd95c1b54d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-10-21 15:34:01 -0600

    Update and rename RandomNumbers.playground to RandomNumbers.swift

--- RandomNumbers.swift
+++ RandomNumbers.swift
@@ -2,17 +2,22 @@
 
 // Swift 5.7
 
-// References
-/* https://stackoverflow.com/questions/24007129/how-does-one-generate-a-random-number-in-swift
+/* References
+
+ Random Numbbers via Stackoverflow
+ https://stackoverflow.com/questions/24007129/how-does-one-generate-a-random-number-in-swift
  
+ Swift Reference Book
  https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html
  
  */
 
 // 1  
+// You use random(in:) to generate random digits from ranges
 let digit = Int.random(in: 0..<10)
 
 // 2
+// randomElement() returns nil if the range is empty, so you unwrap the returned Int? with if let
 if let anotherDigit = (0..<10).randomElement() {
     print(anotherDigit)
 } else {
@@ -20,6 +25,7 @@ if let anotherDigit = (0..<10).randomElement() {
 }
 
 // 3
+// You use random(in:) to generate a random Double, Float or CGFloat and random() to return a random Bool
 let double = Double.random(in: 0..<1)
 let float = Float.random(in: 0..<1)
 //let cgFloat = CGFloat.random(in: 0..<1)
