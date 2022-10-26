93d2d0fd46953ae5530cbefe640f3ea3525c8d10
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-10-26 16:22:44 -0600

    Update LS_01_arrayTuple.swift

--- LS_01_arrayTuple.swift
+++ LS_01_arrayTuple.swift
@@ -1,5 +1,4 @@
 // This script will generate examples of arrays and tuples 
-
 /*
  Made with:
  Swift 5.7
@@ -14,17 +13,20 @@
  Chapter 1
  */
 
-// An array is an ordered list, of only one type, using [ ]
+// An array is an ordered list using [ ]
 // explicitly typed array examples
 let immutable_array : [Int] = [1,2,3]
-print("this is an immutable array which uses let and integer type ", immutable_array)
+print("this is an immutable array which uses keyword let and integer type ", immutable_array)
 
-var string_array : [String] = ["a","b","c"]
-print("this is a mutable array which uses var and string type ",string_array)
+var mutable_array : [String] = ["a","b","c"]
+print("this is a mutable array which uses keyword var and string type ",mutable_array)
 
-// A tuple is a ordered list, which can hold multiple types, using ( )
+var mixed_array : [Any] = [1,"a"]
+print("this is a mixed type array ", mixed_array)
+
+// A tuple is an ordered list, which can hold multiple types, using ( )
 // Tuples are designed for related & interconnected data are often used to return multiple values from a function 
 let serverError = (code:500, message:"Internal Server Error")
-    
+
 print("this is part of a tuple: ", serverError.code)
 print("this is part of a tuple: ", serverError.message)
