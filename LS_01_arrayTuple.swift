2ede8f91ebd4aac2f58893c14db97b139c525352
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-11-06 10:19:15 -0900

    Update LS_01_arrayTuple.swift

--- LS_01_arrayTuple.swift
+++ LS_01_arrayTuple.swift
@@ -1,6 +1,7 @@
 // This script will generate examples of arrays and tuples 
 /*
  Made with:
+ iPad
  Swift 5.7
  Swift Playgrounds 4.1
  */
@@ -13,7 +14,7 @@
  Chapter 1
  */
 
-// An array is an ordered list using [ ]
+// An array is an ordered list using [ ] syntax
 // explicitly typed array examples
 let immutable_array : [Int] = [1,2,3]
 print("this is an immutable array which uses keyword let and integer type ", immutable_array)
@@ -24,7 +25,7 @@ print("this is a mutable array which uses keyword var and string type ",mutable_
 var mixed_array : [Any] = [1,"a"]
 print("this is a mixed type array ", mixed_array)
 
-// A tuple is an ordered list, which can hold multiple types, using ( )
+// A tuple is an ordered list, which can hold multiple types, using ( ) syntax
 // Tuples are designed for related & interconnected data are often used to return multiple values from a function 
 let serverError = (code:500, message:"Internal Server Error")
 
