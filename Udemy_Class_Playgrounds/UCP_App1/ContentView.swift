63dfaa969032eb39f710207f1560d412eb629351
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-11-27 13:39:43 -0900

    Create ContentView.swift

---
+++ Udemy_Class_Playgrounds/UCP_App1/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "mountain.2")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Welcome!")
+        }
+    }
+}
