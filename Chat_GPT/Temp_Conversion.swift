672aeedf767b5d0119159e127fdc058ddf82dfd4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-03-16 21:14:51 -0800

    Update Temp_Conversion.swift

--- Chat_GPT/Temp_Conversion.swift
+++ Chat_GPT/Temp_Conversion.swift
@@ -3,14 +3,13 @@
 // This script was made by Chat GPT
   // https://openai.com/blog/chatgpt
   // March 2023
+
+// Tested using Swift Playgrounds - it worked
   
-// In this example, we first declare the Fahrenheit temperature as a constant of type Double. 
-// We then use the formula to convert the temperature to Celsius and store the result in another
-// constant of type Double called celsius. Finally, we print the original temperature in Fahrenheit
-// and the converted temperature in Celsius using string interpolation.
-
-
-celsius = (fahrenheit - 32) * 5/9
+/* In this example, we first declare the Fahrenheit temperature as a constant of type Double. 
+We then use the formula to convert the temperature to Celsius and store the result in another
+constant of type Double called celsius. Finally, we print the original temperature in Fahrenheit
+and the converted temperature in Celsius using string interpolation. */
 
 let fahrenheit: Double = 68
 let celsius: Double = (fahrenheit - 32) * 5/9
