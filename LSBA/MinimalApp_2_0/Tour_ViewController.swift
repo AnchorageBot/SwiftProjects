aea7eda1ecccd114760b7d764f99d86e97d7d26a
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-07-28 00:52:20 -0800

    Create Tour_ViewController.swift

---
+++ LSBA/MinimalApp_2_0/Tour_ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Reference: Learn Swift by Building Applications - Emil Atanasov
+//
+//  Made with Swift 5.7, Xcode 14.3.1
+//
+//  ViewController.swift
+//  Test App
+//
+//  Each app has at least one ViewController
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
