20a43aa210000312efd46770b87aa970d134fa15
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-07-30 20:05:01 -0800

    Create ContentView.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/CreatingCombiningViews/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 7/30/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->Project
+//  Template->iOS->App
+//  Product Name->Landmarks
+//  Interface->SwiftUI
+//  Language->Swift
+//
+//  Editor->Canvas
+//  Content View->Selectable->Show SwiftUI Inspector
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
