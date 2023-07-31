b6d66cd05cc7458376d59f00b904be99200e05fa
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-07-30 20:06:25 -0800

    Create MapView.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/CreatingCombiningViews/MapView.swift
@@ -0,0 +1,31 @@
+/
+//  MapView.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 7/30/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->SwiftUI View
+//  Editor->Canvas
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    // Create a private state variable to hold region information for the map
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        //  By prefixing a state variable with $, you pass a binding, which is like a reference to the underlying value
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
