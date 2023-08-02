74f41243c5ef7eacbbfce70f59c384e6be6a6141
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-01 19:55:09 -0800

    Create LandmarkRow.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/BuildingListsNav/LandmarkRow.swift
@@ -0,0 +1,38 @@
+//
+//  LandmarkRow.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 7/30/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->SwiftUI View
+//  Editor->Canvas
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
