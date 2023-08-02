c8208dd62005ae116c01d8b6995b7a8ac5da875b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-01 19:52:30 -0800

    Create LandmarksApp.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/BuildingListsNav/LandmarksApp.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarksApp.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 7/30/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->Project
+//  Template->iOS->App
+//  Product Name->Landmarks
+//  Interface->SwiftUI
+//  Language->Swift
+
+import SwiftUI
+
+// The @main attribute identifies the app’s entry point.
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
