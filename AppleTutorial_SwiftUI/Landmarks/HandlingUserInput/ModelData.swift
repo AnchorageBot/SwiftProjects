e7ec82ce008f44ef489f42fb024adf715dca7707
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-02 20:31:37 -0800

    Create ModelData.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/HandlingUserInput/ModelData.swift
@@ -0,0 +1,42 @@
+//
+//  ModelData.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 8/2/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->Swift File
+//
+
+import Foundation
+import Combine
+
+// SwiftUI watches for any changes to observable objects that could affect a view
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+// Creates a load(_:) method that fetches JSON data with a given name from the app’s main bundle
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as (T.self):\n\(error)")
+    }
+}
