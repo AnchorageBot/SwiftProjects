8ebea0afee1cb2e12a4faef67b7e901ce57c3f2e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-04 23:15:24 -0800

    Create RotatedBadgeSymbol.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/DrawPathShape/RotatedBadgeSymbol.swift
@@ -0,0 +1,27 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 8/4/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->SwiftUI View
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+        
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
