239cce810b5170c0e3c0b9597f31bde89b0a38ea
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-06 15:21:04 -0800

    Create HikeDetail.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/AnimateVT/HikeDetail.swift
@@ -0,0 +1,60 @@
+/*
+ Copyright © 2021 Apple Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Abstract:
+A view showing the details for a hike.
+*/
+//
+//  HikeDetail.swift
+//  Landmarks App
+//
+//  Modified with Swift 5.7, Xcode 14.3.1, 8/6/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+
+import SwiftUI
+
+struct HikeDetail: View {
+    let hike: Hike
+    @State var dataToShow = \Hike.Observation.elevation
+
+    var buttons = [
+        ("Elevation", \Hike.Observation.elevation),
+        ("Heart Rate", \Hike.Observation.heartRate),
+        ("Pace", \Hike.Observation.pace)
+    ]
+
+    var body: some View {
+        VStack {
+            HikeGraph(hike: hike, path: dataToShow)
+                .frame(height: 200)
+
+            HStack(spacing: 25) {
+                ForEach(buttons, id: \.0) { value in
+                    Button {
+                        dataToShow = value.1
+                    } label: {
+                        Text(value.0)
+                            .font(.system(size: 15))
+                            .foregroundColor(value.1 == dataToShow
+                                ? .gray
+                                : .accentColor)
+                            .animation(nil)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct HikeDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeDetail(hike: ModelData().hikes[0])
+    }
+}
