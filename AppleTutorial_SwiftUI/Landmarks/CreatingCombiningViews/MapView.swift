5f6126e876e53f710327c98485ba893eb78a536a
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-07 21:16:26 -0800

    Update MapView.swift

--- AppleTutorial_SwiftUI/Landmarks/CreatingCombiningViews/MapView.swift
+++ AppleTutorial_SwiftUI/Landmarks/CreatingCombiningViews/MapView.swift
@@ -15,7 +15,8 @@ import MapKit
 struct MapView: View {
     // Create a private state variable to hold region information for the map
     @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868), 
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
     
     var body: some View {
