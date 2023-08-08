8e0e122e304310ac1bb5be94f78bf4a059310698
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-07 20:46:43 -0800

    Create CategoryHome.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/ComplexInterfaces/CategoryHome.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryHome.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 8/7/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->SwiftUI View
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
