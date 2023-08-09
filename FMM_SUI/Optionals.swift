b8391f50c479e1dddfe645dbbf1964a18ad8a896
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-08 20:03:28 -0800

    Update Optionals.swift

--- FMM_SUI/Optionals.swift
+++ FMM_SUI/Optionals.swift
@@ -9,11 +9,14 @@
 /*
  
 A variable marked as optional may have a value or be empty
+
+To declare an optional, we add a question mark after the type’s name
  
 Sometimes we do not have an initial value to assign to the variable during development
 or need to indicate the absence of a value because the current one becomes invalid
  
-Although optionals seem to work like regular variables, they do not expose their values. 
+Although optionals seem to work like regular variables, they do not expose their values
+
 To read the value of an optional, we must unwrap it by adding an exclamation mark at the end of the name
  
  */
