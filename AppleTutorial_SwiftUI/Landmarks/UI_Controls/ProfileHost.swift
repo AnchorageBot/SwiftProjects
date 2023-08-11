ce1a820a16a1286c2031595b66219f9f0f3f0bb6
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-10 20:50:30 -0800

    Update ProfileHost.swift

--- AppleTutorial_SwiftUI/Landmarks/UI_Controls/ProfileHost.swift
+++ AppleTutorial_SwiftUI/Landmarks/UI_Controls/ProfileHost.swift
@@ -1,8 +1,8 @@
-//
+/
 //  ProfileHost.swift
 //  Landmarks App
 //
-//  Made with Swift 5.7, Xcode 14.3.1, 8/8/2023
+//  Made with Swift 5.7, Xcode 14.3.1, 8/10/2023
 //  Ref: https //developer.apple.com/tutorials/swiftui
 //
 //  Xcode->File->New->File->SwiftUI View
@@ -11,11 +11,34 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+                
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
         }
         .padding()
     }
@@ -24,5 +47,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
