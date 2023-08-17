7e01faaaf9144a32869f1ab5b66ebbfe9c79ea3d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-17 15:30:12 -0800

    Create FeatureCard.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/UI_Kit/FeatureCard.swift
@@ -0,0 +1,55 @@
+//
+//  FeatureCard.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 8/12/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->SwiftUI View
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+            .overlay {
+                TextOverlay(landmark: landmark)
+            }
+    }
+}
+
+struct TextOverlay: View {
+    var landmark: Landmark
+    
+    var gradient: LinearGradient {
+        .linearGradient(
+            Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
+            startPoint: .bottom,
+            endPoint: .center)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            gradient
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .bold()
+                Text(landmark.park)
+            }
+            .padding()
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard(landmark: ModelData().features[0])
+    }
+}
