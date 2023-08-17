9d5690214043c9460838009852fc8e0cc73c48ea
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-17 15:15:35 -0800

    Create Badge.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/UI_Kit/Badge.swift
@@ -0,0 +1,40 @@
+//
+//  Badge.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 8/4/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->SwiftUI View
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
+        }
+            .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
