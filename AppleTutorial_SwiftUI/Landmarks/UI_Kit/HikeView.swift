4d961e91f562dc5c407ed9b0e9fa876a3e4a9465
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-17 15:23:25 -0800

    Create HikeView.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/UI_Kit/HikeView.swift
@@ -0,0 +1,86 @@
+/*
+ Copyright © 2021 Apple Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Abstract:
+A view displaying information about a hike, including an elevation graph.
+*/
+//
+//  HikeView.swift
+//  Landmarks App
+//
+//  Modified with Swift 5.7, Xcode 14.3.1, 8/6/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+
+import SwiftUI
+
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        //AnyTransition.slide
+        //AnyTransition.move(edge: .trailing)
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                }
+
+                Spacer()
+
+                Button {
+                    //withAnimation(.easeInOut(duration: 4)) {
+                    withAnimation {
+                        showDetail.toggle()
+                    }
+                } label: {
+                    Label("Graph", systemImage: "chevron.right.circle")
+                        .labelStyle(.iconOnly)
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        //.animation(nil, value: showDetail)
+                        .scaleEffect(showDetail ? 1.5 : 1)
+                        .padding()
+                        //.animation(.spring(), value: showDetail)
+                }
+            }
+
+            if showDetail {
+                HikeDetail(hike: hike)
+                    //.transition(.slide)
+                    .transition(.moveAndFade)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HikeView(hike: ModelData().hikes[0])
+                .padding()
+            Spacer()
+        }
+    }
+}
