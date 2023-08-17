46202a2c9935031994f0e096f5a3fc81bda21152
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-17 15:26:24 -0800

    Create Landmark.swift

---
+++ AppleTutorial_SwiftUI/Landmarks/UI_Kit/Landmark.swift
@@ -0,0 +1,54 @@
+//
+//  Landmark.swift
+//  Landmarks App
+//
+//  Made with Swift 5.7, Xcode 14.3.1, 8/12/2023
+//  Ref: https //developer.apple.com/tutorials/swiftui
+//
+//  Xcode->File->New->File->Swift File
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    // you can read the value associated with the key by creating a new property with the same name as the key
+    var isFavorite: Bool
+    var isFeatured: Bool
+    
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+    
+    //  property is private because users care only about image itself
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    // returns the feature image, if it exists
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
