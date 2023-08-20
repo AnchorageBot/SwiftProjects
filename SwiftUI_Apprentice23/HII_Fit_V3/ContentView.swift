3a228ddd0b668068cb42a84c2cda70739c3d6679
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-19 22:49:23 -0800

    Create ContentView.swift

---
+++ SwiftUI_Apprentice23/HII_Fit_V3/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  HII_Fit App
+//
+//  Created on 8/19/23
+//
+//  Command N -> Swift UI
+//
+//  Used to define the app's organization
+//
+
+import SwiftUI
+
+// struct = named data type with properties and methods
+// name of the first structure matches the name of the file
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+
+            WelcomeView()
+            
+            ForEach(0..<4) { number in
+                ExerciseView(index: number)
+            }
+        }
+        // swiping with dots
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(
+            PageIndexViewStyle(backgroundDisplayMode: .always))
+
+        // swiping without dots
+//        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
