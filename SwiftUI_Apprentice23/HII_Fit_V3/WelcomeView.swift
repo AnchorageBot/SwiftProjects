029449180e95ab0d84a852e86fa925504b12ca84
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-19 22:49:54 -0800

    Create WelcomeView.swift

---
+++ SwiftUI_Apprentice23/HII_Fit_V3/WelcomeView.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeView.swift
+//  HII_Fit
+//
+//  Created on 8/19/23
+//
+//  Command N -> Swift UI
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Let's Go")
+            .font(.largeTitle)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
