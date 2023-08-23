33e186968ec13354b514a705903bef77ae76b178
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-22 19:17:37 -0800

    Create LS_03_guard.swift

---
+++ LS/LS_03_guard.swift
@@ -0,0 +1,31 @@
+
+/*
+ 
+ LS_03_guard.swift
+ 
+ Swift Playgrounds 4.1, Swift 5.7
+ 
+ Made on 8/22/2023
+ 
+ Reference:
+ 
+ Learning Swift: Building Apps for MacOS, iOS, and Beyond, Manning et al 
+ 
+
+The guard keyword lets you define a test that needs to pass; alternatively, if it 
+doesn’t pass, a different block of code is run. 
+
+*/
+
+func doAThing(){
+    guard 2+2 == 4 else {
+        print("The universe makes no sense")
+        return
+    }
+    print("We can continue with our daily lives")
+}
+
+doAThing()
+
+// We can continue with our daily lives
+
