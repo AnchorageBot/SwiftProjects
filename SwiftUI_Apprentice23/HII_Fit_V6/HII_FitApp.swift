24330ceba02bd5919977ee91e52402c1a7e4aa5b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-27 13:28:49 -0800

    Create HII_FitApp.swift

---
+++ SwiftUI_Apprentice23/HII_Fit_V6/HII_FitApp.swift
@@ -0,0 +1,21 @@
+//
+//  HII_FitApp.swift
+//  HII_Fit
+//
+//  Created on 8/19/23
+//  Made with Swift 5.7, Xcode 14.3.1
+//
+//  Command N -> Swift UI
+//
+/*  Used to launch app  */
+
+import SwiftUI
+
+@main
+struct HII_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // root view
+        }
+    }
+}
