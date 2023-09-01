b788b6747ee48c0f2d441b648a7816debd905610
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-08-31 18:23:17 -0800

    Create LearnSwiftUIApp.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ExploreStructure/LearnSwiftUIApp.swift
@@ -0,0 +1,34 @@
+/*
+
+  LearnSwiftUIApp.swift
+  LearnSwiftUI
+
+  Created on 8/31/23
+  Made with Swift 5.8, Xcode 14.3.1
+
+  https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+  Xcode->File->New Project -> iOS - App
+ 
+*/
+
+import SwiftUI
+
+// entry point responsible for the start-up of the app and there can only be one
+@main
+
+// The structure implements the computed property body
+struct LearnSwiftUIApp: App {
+
+    // returns the contents of your app described as a Scene
+    var body: some Scene {
+
+        // WindowGroup scene to represent the main window that the app displays
+        WindowGroup {
+
+            // custom view that creates a view hierarchy that consists of an image and text
+            ContentView()
+
+        }
+    }
+}
