cbb0e72037cb79903c3a43771f09d6bef708f301
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 14:04:12 -0800

    Create macOSView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V2/macOSView.swift
@@ -0,0 +1,31 @@
+/*
+
+ macOSView.swift
+ LearnSwiftUI
+
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/2/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct macOSView: View {
+    var body: some View {
+        Text("This is the macOS view")
+    }
+}
+
+struct macOSView_Previews: PreviewProvider {
+    static var previews: some View {
+        macOSView()
+    }
+}
