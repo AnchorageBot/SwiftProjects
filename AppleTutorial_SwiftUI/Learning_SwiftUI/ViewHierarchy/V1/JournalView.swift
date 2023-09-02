adc05f75e688c11211cec6312b6af80b0131e4b3
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 09:30:03 -0800

    Create JournalView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/JournalView.swift
@@ -0,0 +1,25 @@
+/*
+
+ JournalView.swift
+ LearnSwiftUI
+
+ Created on 9/2/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+ Splat N -> Swift UI View
+
+*/
+
+import SwiftUI
+
+struct JournalView: View {
+    var body: some View {
+        Text("Journal")
+    }
+}
+
+struct JournalView_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalView()
+    }
+}
