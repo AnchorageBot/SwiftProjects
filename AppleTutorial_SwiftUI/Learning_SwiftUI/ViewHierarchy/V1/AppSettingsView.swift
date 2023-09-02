81e75620d72c2be616528221b1b415a9b8d9358d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 09:30:38 -0800

    Create AppSettingsView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/AppSettingsView.swift
@@ -0,0 +1,25 @@
+/*
+
+ AppSettingsView.swift
+ LearnSwiftUI
+
+ Created on 9/2/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+ Splat N -> Swift UI View
+
+ */
+
+import SwiftUI
+
+struct AppSettingsView: View {
+    var body: some View {
+        Text("App Settings")
+    }
+}
+
+struct AppSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppSettingsView()
+    }
+}
