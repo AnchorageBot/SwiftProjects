5f79f27a9c051b89da33b10fac88c5543fcd8f7c
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 10:26:27 -0800

    Update AppSettingsView.swift

--- AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/AppSettingsView.swift
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/AppSettingsView.swift
@@ -4,7 +4,7 @@
  LearnSwiftUI
 
  How to create this file with keyboard and Xcode commands:
- Splat N -> Swift UI View
+ Command Key (Splat) N -> Swift UI View
 
  Created on 9/2/2023
  Made with Swift 5.8, Xcode 14.3.1
