80b28c4348f1c49e8e382e98ff45f33d458fde9b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 14:02:10 -0800

    Create ontentView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V2/ontentView.swift
@@ -0,0 +1,37 @@
+/*
+
+ ContentView.swift
+ LearnSwiftUI
+
+ How to create this file with Xcode commands:
+ Xcode->File->New Project -> iOS - App
+
+ Created on 8/31/23, updated on 9/2/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ In SwiftUI, a scene contains the view hierarchy that an app displays as its user interface. A view hierarchy defines the layout of views relative to other views.
+
+*/
+
+import SwiftUI
+
+// ContentView is a structure that conforms to the View protocol
+
+// A view defines one or more visual elements that appear somewhere on a screen. A view is typically made up of other views, creating a view hierarchy.
+
+struct ContentView: View {
+
+    // returns the contents of your app described as a Scene
+    var body: some View {
+        
+        // Root node of the view hierarchy
+        TabView {
+            
+        }
+    }
+}
