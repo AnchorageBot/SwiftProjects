817fd1d5aa8328da5a368cca67efdce7fb342bbe
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 10:26:10 -0800

    Update JournalView.swift

--- AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/JournalView.swift
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/JournalView.swift
@@ -3,11 +3,11 @@
  JournalView.swift
  LearnSwiftUI
 
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
  Created on 9/2/2023
  Made with Swift 5.8, Xcode 14.3.1
-
- How to create this file with keyboard and Xcode commands:
- Splat N -> Swift UI View
 
 */
 
