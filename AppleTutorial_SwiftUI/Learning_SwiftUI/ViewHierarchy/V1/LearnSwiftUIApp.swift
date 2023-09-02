0f9f1926cddf9f18f83ebd846c72443726f84669
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 10:12:34 -0800

    Update LearnSwiftUIApp.swift

--- AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/LearnSwiftUIApp.swift
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V1/LearnSwiftUIApp.swift
@@ -9,7 +9,7 @@
   Reference:
   https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
 
-  How to create this file:
+  How to create this file with Xcode commands:
   Xcode->File->New Project -> iOS - App
 
   An app structure describes the content and behavior of your app, and each SwiftUI
