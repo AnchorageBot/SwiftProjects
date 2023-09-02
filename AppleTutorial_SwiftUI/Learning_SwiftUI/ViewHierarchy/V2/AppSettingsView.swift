fe3bad1f8aa5f6caaf3e4bf177177449f22fdd8c
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-02 14:02:54 -0800

    Create AppSettingsView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/ViewHierarchy/V2/AppSettingsView.swift
@@ -0,0 +1,26 @@
+/*
+
+ AppSettingsView.swift
+ LearnSwiftUI
+
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
+ Created on 9/2/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct AppSettingsView: View {
+    var body: some View {
+        Text("App Settings")
+    }
+}
+
+struct AppSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppSettingsView()
+    }
+}
