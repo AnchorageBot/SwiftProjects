8be154c4095c521f5f593f911df8c08abd7d2a40
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-07 07:27:52 -0800

    Create LearnSwiftUI_V2App.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/LayerContent/V4/LearnSwiftUI_V2App.swift
@@ -0,0 +1,30 @@
+/*
+
+ LearnSwiftUI_V2App.swift
+ LearnSwiftUI_V2
+
+ How to create this file with Xcode commands:
+ Xcode->File->New Project -> iOS - App
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/layering-content
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/7/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+@main
+struct LearnSwiftUI_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
