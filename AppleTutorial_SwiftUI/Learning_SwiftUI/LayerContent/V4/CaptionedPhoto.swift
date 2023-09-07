43f54657f9049c96124e6fc73572d068aa9b0d4e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-07 07:28:26 -0800

    Create CaptionedPhoto.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/LayerContent/V4/CaptionedPhoto.swift
@@ -0,0 +1,77 @@
+/*
+
+ CaptionedPhoto.swift
+ LearnSwiftUI_V2
+
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/layering-content
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/7/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct CaptionedPhoto: View {
+
+    let assetName: String
+    let captionText: String
+
+    var body: some View {
+//        Image(assetName)
+          Image("Image")
+            .resizable()
+            .scaledToFit()
+            .overlay(alignment: .bottom) {
+                Caption(text: captionText)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+            .padding()
+    }
+}
+
+struct Caption: View {
+
+    let text: String
+
+    var body: some View {
+        Text(text)
+            .padding()
+            .background(Color("TextContrast").opacity(0.75),
+                        in: RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+            .padding()
+    }
+}
+
+struct CaptionedPhoto_Previews: PreviewProvider {
+
+    static let landscapeName = "Landscape.jpeg"
+    static let landscapeCaption = "This photo is wider than it is tall"
+    static let portraitName = "Portrait.jpeg"
+    static let portraitCaption = "This photo is taller than it is wide"
+
+    static var previews: some View {
+
+        CaptionedPhoto(assetName: portraitName,
+                            captionText: portraitCaption)
+        .preferredColorScheme(.dark)
+
+        CaptionedPhoto(assetName: landscapeName,
+                           captionText: landscapeCaption)
+        .preferredColorScheme(.dark)
+
+//        CaptionedPhoto(assetName: landscapeName,
+//                            captionText: landscapeCaption)
+//        .preferredColorScheme(.light)
+
+
+    }
+}
