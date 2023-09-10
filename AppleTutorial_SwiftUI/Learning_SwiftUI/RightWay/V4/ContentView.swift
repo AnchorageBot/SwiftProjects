15ee90e03fbb2d0cb6a78fd9cf5af36a1925c94e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-10 06:44:38 -0800

    Create ContentView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/RightWay/V4/ContentView.swift
@@ -0,0 +1,38 @@
+/*
+
+ ContentView.swift
+ LearnSwiftUI_V3
+
+ How to create this file with Xcode commands:
+ Xcode -> File -> New Project -> iOS - App
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/choosing-the-right-way-to-hide-a-view
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/10/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        
+        TrainCars()
+            .frame(width: 350)
+            .preferredColorScheme(.dark)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
