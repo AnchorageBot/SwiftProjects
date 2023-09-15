0254d793d440c1b7dd05ca22e0daae6309d6e01d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-15 04:22:47 -0800

    Create LearnSwiftUI_V5App.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V1/LearnSwiftUI_V5App.swift
@@ -0,0 +1,30 @@
+/*
+
+ LearnSwiftUI_V5App.swift
+ LearnSwiftUI_V5
+
+ How to create this file with Xcode commands:
+ Xcode -> File -> New Project -> iOS - App
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/adjusting-the-space-between-views
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/15/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+@main
+struct LearnSwiftUI_V5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
