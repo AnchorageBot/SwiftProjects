079a7b19bd837e2ac1c0252a65de4ad1cfd49b3c
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-16 08:31:17 -0800

    Create ScaledSpacing.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V3/ScaledSpacing.swift
@@ -0,0 +1,56 @@
+/*
+
+ ScaledSpacing.swift
+ LearnSwiftUI_V5
+
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/adjusting-the-space-between-views
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/SwiftUI/Image
+
+ Created on 9/16/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct ScaledSpacing: View {
+    @ScaledMetric var trainCarSpace = 5
+
+    var body: some View {
+
+        VStack {
+
+            Text("Scaled Spacing")
+
+            HStack(spacing:trainCarSpace) {
+                TrainCar(.front)
+                TrainCar(.middle)
+                TrainCar(.rear)
+            }
+
+            .padding()
+
+            HStack(spacing:trainCarSpace) {
+                TrainCar(.rear)
+                TrainCar(.middle)
+                TrainCar(.front)
+            }
+
+        }
+    }
+}
+
+struct ScaledSpacing_Previews: PreviewProvider {
+    static var previews: some View {
+        ScaledSpacing()
+    }
+}
