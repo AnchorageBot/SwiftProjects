5453614332c79594dcf4900bd968744bfa4a5350
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-16 09:08:26 -0800

    Create DefaultPadding.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V3/DefaultPadding.swift
@@ -0,0 +1,51 @@
+/*
+
+ DefaultPadding.swift
+ LearnSwiftUI_V5
+
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/adjusting-the-space-between-views
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/SwiftUI/Image
+
+ Created on 9/16/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct DefaultPadding: View {
+
+    var body: some View {
+
+        Text("Default Padding Example")
+        
+        HStack {
+            TrainCar(.rear)
+            TrainCar(.middle)
+                .padding()
+                // this was not working
+                .background(Color("customBlue"))
+                // this works
+                .background(.teal)
+            TrainCar(.front)
+        }
+
+        TrainTrack()
+
+    }
+}
+
+struct DefaultPadding_Previews: PreviewProvider {
+    static var previews: some View {
+        DefaultPadding()
+    }
+}
