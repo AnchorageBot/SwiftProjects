b38e8c1217375710ac9b35dccc4dad6321915d41
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-16 08:42:33 -0800

    Update ScaledSpacing.swift

--- AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V3/ScaledSpacing.swift
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V3/ScaledSpacing.swift
@@ -29,7 +29,7 @@ struct ScaledSpacing: View {
 
         VStack {
 
-            Text("Scaled Spacing")
+            Text("Scaled Spacing Example")
 
             HStack(spacing:trainCarSpace) {
                 TrainCar(.front)
