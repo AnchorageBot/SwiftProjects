9195ec1814a7f642b464ca8f1479305916676ac4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-16 08:41:16 -0800

    Create ZeroSpacing.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V3/ZeroSpacing.swift
@@ -0,0 +1,44 @@
+/*
+
+ ZeroSpacing.swift
+ LearnSwiftUI_V5
+
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/adjusting-the-space-between-views
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/SwiftUI/Image
+
+ Created on 9/16/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct ZeroSpacing: View {
+
+    var body: some View {
+
+        Text("Zero Spacing Example")
+
+        HStack(spacing: 0) {
+            TrainCar(.rear)
+            TrainCar(.middle)
+            TrainCar(.front)
+        }
+        TrainTrack()
+    }
+}
+
+struct ZeroSpacing_Previews: PreviewProvider {
+    static var previews: some View {
+        ZeroSpacing()
+    }
+}
