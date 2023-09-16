4e95c67306e78fea281b69ec11a37a9b353dbefb
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-16 08:19:15 -0800

    Create ContentView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V3/ContentView.swift
@@ -0,0 +1,47 @@
+/*
+
+ ContentView.swift
+ LearnSwiftUI_V5
+
+ How to create this file with Xcode commands:
+ Xcode -> File -> New Project -> iOS - App
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/adjusting-the-space-between-views
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/15/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+
+//        TrainCar(.front)
+//            .frame(width: 350)
+//            .preferredColorScheme(.dark)
+
+//        Train_Cars()
+//            .preferredColorScheme(.dark)
+
+//        DefaultSpacing()
+//            .preferredColorScheme(.dark)
+
+        SpecificSpacing()
+            .preferredColorScheme(.dark)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
