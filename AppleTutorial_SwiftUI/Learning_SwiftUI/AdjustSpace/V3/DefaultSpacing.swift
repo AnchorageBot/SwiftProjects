85d23cd9487702325e9777da7b63ff2932a75765
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-16 08:21:01 -0800

    Create DefaultSpacing.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/AdjustSpace/V3/DefaultSpacing.swift
@@ -0,0 +1,44 @@
+/*
+
+ DefaultSpacing.swift
+ LearnSwiftUI_V5
+
+ How to create this file with keyboard and Xcode commands:
+ Command Key (Splat) N -> Swift UI View
+
+ References:
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+
+ https://developer.apple.com/tutorials/swiftui-concepts/adjusting-the-space-between-views
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/SwiftUI/Image
+
+ Created on 9/16/2023
+ Made with Swift 5.8, Xcode 14.3.1
+
+*/
+
+import SwiftUI
+
+struct DefaultSpacing: View {
+
+    var body: some View {
+
+        Text("Default Spacing")
+
+        HStack {
+            TrainCar(.rear)
+            TrainCar(.middle)
+            TrainCar(.front)
+        }
+        TrainTrack()
+    }
+}
+
+struct DefaultSpacing_Previews: PreviewProvider {
+    static var previews: some View {
+        DefaultSpacing()
+    }
+}
