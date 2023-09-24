9162bf0e768d2b5becca80d77b16e36c03618225
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-24 05:44:05 -0800

    Create RecipeNotSelectedView.swift

---
+++ AppleTutorial_SwiftUI/Learning_SwiftUI/StateBindings/V2/RecipeNotSelectedView.swift
@@ -0,0 +1,16 @@
+/*
+
+ Source:
+ https://developer.apple.com/tutorials/swiftui-concepts/driving-changes-in-your-ui-with-state-and-bindings
+
+ */
+
+import SwiftUI
+
+struct RecipeNotSelectedView: View {
+
+    var body: some View {
+
+        Text("Select a recipe.")
+    }
+}
