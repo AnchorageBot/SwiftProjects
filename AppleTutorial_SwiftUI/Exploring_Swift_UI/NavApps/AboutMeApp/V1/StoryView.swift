d24de969d15411b59288a8ce6e26bc038ad1d1ee
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-27 21:40:22 -0800

    Create StoryView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V1/StoryView.swift
@@ -0,0 +1,47 @@
+/*
+
+ StoryView.swift
+ Explore_SwiftUI_V1
+
+ How to create a View file with keyboard and Xcode commands:
+
+    * Command Key (Splat) N -> Swift UI View
+
+ References:
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/27/2023 with Swift 5.9, Xcode 15.0
+
+*/
+
+
+import SwiftUI
+
+struct StoryView: View {
+
+    var body: some View {
+
+        VStack {
+
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            ScrollView {
+                Text(information.story)
+                    .font(.body)
+                    .padding()
+            }
+        }
+
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
