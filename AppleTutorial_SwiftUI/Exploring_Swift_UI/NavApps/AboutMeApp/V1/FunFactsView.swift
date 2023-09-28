f3c50b0fd69cc0be1f5b6e51bd8e6ac927717aef
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-27 21:41:33 -0800

    Create FunFactsView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V1/FunFactsView.swift
@@ -0,0 +1,51 @@
+/*
+
+ FunFactsView.swift
+ Explore_SwiftUI_V1
+
+ How to create a View file with keyboard and Xcode commands:
+
+    * Command Key (Splat) N -> Swift UI View
+
+ References:
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/27/2023 with Swift 5.9, Xcode 15.0
+
+*/
+
+import SwiftUI
+
+struct FunFactsView: View {
+
+    @State private var funFact = ""
+
+    var body: some View {
+
+        VStack {
+
+            Text("Fun Facts")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text(funFact)
+                .padding()
+                .font(.title)
+                .frame(minHeight: 400)
+
+            Button("Show Random Fact") {
+                funFact = information.funFacts.randomElement()!
+            }
+        }
+
+        .padding()
+
+    }
+}
+
+struct FunFactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FunFactsView()
+    }
+}
