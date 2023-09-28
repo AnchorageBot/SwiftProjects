9eab55085b413275d5d2b7ed15ac6b709a88aeb4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-27 21:42:06 -0800

    Create Data.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V1/Data.swift
@@ -0,0 +1,42 @@
+/*
+
+ Data.swift
+ Explore_SwiftUI_V1
+
+ How to create a View file with keyboard and Xcode commands:
+
+    * Command Key (Splat) N -> Swift UI View
+
+ References:
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ Created on 9/27/2023 with Swift 5.9, Xcode 15.0
+
+*/
+
+import Foundation
+import SwiftUI
+
+struct Info {
+    let image: String
+    let name: String
+    let story: String
+    let hobbies: [String]
+    let foods: [String]
+    let colors: [Color]
+    let funFacts: [String]
+}
+
+let information = Info(
+    image: "Placeholder",
+    name: "My Name",
+    story: "It's a good story",
+    hobbies: ["Fun stuff", "More fun stuff"],
+    foods: ["🥐", "🌮", "🍣"],
+    colors: [Color.red, Color.green, Color.blue],
+    funFacts: ["Einstein is often right",
+              "Antimatter is subject to gravity",
+              "The photoelectric effect is real",
+    ]
+)
