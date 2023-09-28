88bb67371ab018b9ac2eafe1dbfba39eaca91a29
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-27 21:39:41 -0800

    Create ContentView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V1/ContentView.swift
@@ -0,0 +1,63 @@
+/*
+
+ ContentView.swift
+ Explore_SwiftUI_V1
+
+ How to create this project with Xcode commands:
+
+    * Xcode-> File-> New Project -> iOS - App
+
+ This creates two files
+
+    * ContentView.swift
+    * ProjectName_App.swift
+
+ References:
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/SwiftUI/View/body-8kl5o
+
+ https://developer.apple.com/documentation/SwiftUI/TabView
+
+ https://developer.apple.com/documentation/SwiftUI/Label
+
+ Created on 9/27/2023 with Swift 5.9, Xcode 15.0
+
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+
+    // The content and behavior of the view
+    var body: some View {
+
+        // Creates a user interface with tabs
+        TabView {
+
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "person")
+                }
+
+            StoryView()
+                .tabItem {
+                    Label("Story", systemImage: "book")
+                }
+
+            FavoritesView()
+                .tabItem {
+                    Label("Fun Facts", systemImage: "hand.thumbsup")
+                }
+
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
