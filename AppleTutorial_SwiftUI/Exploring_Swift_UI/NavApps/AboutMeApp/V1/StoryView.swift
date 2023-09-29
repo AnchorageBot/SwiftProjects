68ee1a979ab549544bb224f311034ff9b47350af
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-28 18:26:10 -0800

    Update StoryView.swift

--- AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V1/StoryView.swift
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V1/StoryView.swift
@@ -12,6 +12,7 @@
  https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
 
  Created on 9/27/2023 with Swift 5.9, Xcode 15.0
+ Updated on 9/28/2023
 
 */
 
@@ -37,6 +38,7 @@ struct StoryView: View {
         }
 
         .padding([.top, .bottom], 50)
+        .preferredColorScheme(.dark)
     }
 }
 
