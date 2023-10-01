c6aff6525e0a3720d8662b9407f021251fc967db
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-30 16:22:41 -0800

    Create Data.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V2/Data.swift
@@ -0,0 +1,35 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import Foundation
+import SwiftUI
+
+struct Info {
+    let image: String
+    let name: String
+    let story: String
+    let hobbies: [String]
+    let foods: [String]
+    let colors: [Color]
+    let funFacts: [String]
+}
+
+let information = Info(
+    image: "Placeholder",
+    name: "My Name",
+    story: "A story can be about anything you can dream up. There are no right answers, there is no one else.\n\nNeed some inspiration?\n• 🐶🐱🛶️🎭🎤🎧🎸\n• 🏄‍♀️🚵‍♀️🚴‍♀️⛵️🥾🏂⛷📚\n• ✍️🥖☕️🏋️‍♂️🚲🧗‍♀️ ",
+    hobbies: ["bicycle", "ticket.fill", "book.fill"],
+    foods: ["🥐", "🌮", "🍣"],
+    colors: [Color.blue, Color.purple, Color.pink],
+    funFacts: [
+        "The femur is the longest and largest bone in the human body.",
+        "The moon is 238,900 miles away.",
+        "Prince’s last name was Nelson.",
+        "503 new species were discovered in 2020.",
+        "Ice is 9 percent less dense than liquid water.",
+        "You can spell every number up to 1,000 without using the letter A.\n\n...one, two, three, four...ninety-nine...nine hundred ninety-nine 🧐",
+        "A collection of hippos is called a bloat.",
+        "White sand beaches are made of parrotfish poop.",
+    ]
+)
