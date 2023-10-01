15b4cc80cdb3bf542aecd584535439cf0b9b0317
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-30 16:25:31 -0800

    Create StoryView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V2/StoryView.swift
@@ -0,0 +1,29 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            ScrollView {
+                Text(information.story)
+                    .font(.body)
+                    .padding()
+            }
+        }
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
