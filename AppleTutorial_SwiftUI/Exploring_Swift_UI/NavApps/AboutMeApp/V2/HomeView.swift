c8967af856989b7dd11b66b06200243ad79f830d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-30 16:24:50 -0800

    Create HomeView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V2/HomeView.swift
@@ -0,0 +1,33 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack {
+            Text("All About")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            Image(information.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(10)
+                .padding(40)
+
+            Text(information.name)
+                .font(.title)
+        }
+    }
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
