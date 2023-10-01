63381b58e43d147e86fd6114c2bac573081f116d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-30 16:23:24 -0800

    Create FavoritesView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V2/FavoritesView.swift
@@ -0,0 +1,59 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        VStack {
+            Text("Favorites")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 40)
+            
+            Text("Hobbies")
+                .font(.title2)
+            
+            HStack {
+                ForEach(information.hobbies, id: \.self) { hobby in
+                    Image(systemName: hobby)
+                        .resizable()
+                        .frame(maxWidth: 80, maxHeight: 60)
+                    
+                }
+                .padding()
+            }
+            .padding()
+
+            Text("Foods")
+                .font(.title2)
+            
+            HStack(spacing: 60) {
+                ForEach(information.foods, id: \.self) { food in
+                    Text(food)
+                        .font(.system(size: 48))
+                }
+            }
+            .padding()
+
+            Text("Favorite Colors")
+                .font(.title2)
+
+            HStack(spacing: 30) {
+                ForEach(information.colors, id: \.self) { color in
+                    color
+                        .frame(width: 70, height: 70)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
