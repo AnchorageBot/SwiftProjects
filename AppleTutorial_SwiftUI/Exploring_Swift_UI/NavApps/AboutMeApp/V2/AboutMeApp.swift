20222e096e4ee643d2110b0a246067e36193182f
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-09-30 16:20:54 -0800

    Create AboutMeApp.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/AboutMeApp/V2/AboutMeApp.swift
@@ -0,0 +1,14 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
