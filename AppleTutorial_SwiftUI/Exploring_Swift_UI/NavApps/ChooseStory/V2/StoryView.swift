d672f7c8fbe20f64bd0999ef44dcfc2d41d2846b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-04 20:08:22 -0800

    Create StoryView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/ChooseStory/V2/StoryView.swift
@@ -0,0 +1,20 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct StoryView: View {
+
+    var body: some View {
+        NavigationStack {
+            StoryPageView(story: story, pageIndex: 0)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
