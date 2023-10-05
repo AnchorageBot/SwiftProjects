1926e7ef8bf9a91ab801e23149c208e69f03578b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-04 20:06:56 -0800

    Create StoryApp.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/ChooseStory/V2/StoryApp.swift
@@ -0,0 +1,14 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StoryView()
+        }
+    }
+}
