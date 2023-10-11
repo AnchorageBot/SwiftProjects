78883b2b42894ab8a237ce07f13646647cf37ea5
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-10 21:01:40 -0800

    Update EventData.swift

--- AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/DatePlanner/V1/EventData.swift
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/DatePlanner/V1/EventData.swift
@@ -21,6 +21,14 @@
 
  https://developer.apple.com/documentation/foundation/date
 
+ https://developer.apple.com/documentation/swift/using-imported-c-functions-in-swift
+
+ https://developer.apple.com/documentation/swiftui/binding
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/enumerations/
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/controlflow/#Switch
+
  Created on 10/9/2023 with Swift 5.9, Xcode 15.0
 
  The EventData observable object populates the data in your event list
@@ -90,8 +98,111 @@ class EventData: ObservableObject {
               ],
               date: Date.roundedHoursFromNow(60 * 60 * 24 * 365 * 2)),
 
-        E
+        Event(symbol: "graduationcap.fill",
+              color: .primary,
+              title: "First Day of School",
+              tasks: [
+                EventTask(text: "Notebooks"),
+                EventTask(text: "Pencils"),
+                EventTask(text: "Binder"),
+                EventTask(text: "First day of school outfit"),
+              ],
+              date: Date.roundedHoursFromNow(60 * 60 * 24 * 365)),
+
+        Event(symbol: "book.fill",
+              color: .purple,
+              title: "Book Launch",
+              tasks: [
+                EventTask(text: "Finish first draft"),
+                EventTask(text: "Send draft to editor"),
+                EventTask(text: "Final read-through"),
+              ],
+              date: Date.roundedHoursFromNow(60 * 60 * 24 * 365 * 2)),
+
+        Event(symbol: "case.fill",
+              color: .orange,
+              title: "Sayulita Trip",
+              tasks: [
+                EventTask(text: "Buy plane tickets"),
+                EventTask(text: "Get a new bathing suit"),
+                EventTask(text: "Find a hotel room"),
+              ],
+              date: Date.roundedHoursFromNow(60* 60 * 24 * 19)),
     ]
+
+    func delete(_ event: Event) {
+        events.removeAll { $0.id == event.id }
+    }
+
+    func add(_ event: Event) {
+        events.append(event)
+    }
+
+    func exists(_ event: Event) -> Bool {
+        events.contains(event)
+    }
+
+    func sortedEvents(period: Period) -> Binding<[Event]> {
+        Binding<[Event]>(
+            get: {
+                self.events
+                    .filter {
+                        switch period {
+                        case .nextSevenDays:
+                            return $0.isWithinSevenDays
+                        case .nextThirtyDays:
+                            return $0.isWithinSevenToThirtyDays
+                        case .future:
+                            return $0.isDistant
+                        case .past
+                            return $0.isPast
+                        }
+                    }
+                    .sorted { $0.date < $1.date }
+            },
+            set: { events in
+                for event in events {
+                    if let index = self.events.firstIndex(where: { $0.id == event.id }) {
+                        self.events[index] = event
+                    }
+                }
+            }
+        )
+    }
+
 }
 
+enum Period: String, CaseIterable, Identifiable {
+    case nextSevenDays = "Next 7 Days"
+    case nextThirtyDays = "Next 30 Days"
+    case future = "Future"
+    case past = "Past"
 
+    var id: String { self.rawValue }
+    var name: String { self.rawValue }
+}
+
+extension Date {
+
+    static func from(month: Int, day: Int, year: Int) -> Date {
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+
+        let calendar = Calendar(identifier: .gregorian)
+        if let date = calendar.date(from: dateComponents) {
+            return date
+        } else {
+            return Date()
+        }
+    }
+
+    static func roundedHoursFromNow(_ hours: Double) -> Date {
+        let exactDate = Date(timeIntervalSinceNow: hours)
+        guard let hourRange = Calendar.current.dateInterval(of: .hour, for: exactDate) else {
+            return exactDate
+        }
+        return hourRange.end
+    }
+}
