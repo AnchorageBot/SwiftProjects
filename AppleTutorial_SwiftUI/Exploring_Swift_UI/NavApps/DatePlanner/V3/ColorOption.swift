e05024ba94b1867c6b9cd5f53768d4d07a240f72
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-15 11:49:49 -0800

    Create ColorOption.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/DatePlanner/V3/ColorOption.swift
@@ -0,0 +1,31 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct ColorOptions {
+    static var all: [Color] = [
+        .primary,
+        .gray,
+        .red,
+        .orange,
+        .yellow,
+        .green,
+        .mint,
+        .cyan,
+        .indigo,
+        .purple,
+    ]
+    
+    static var `default` : Color = Color.primary
+    
+    static func random() -> Color {
+        if let element = ColorOptions.all.randomElement() {
+            return element
+        } else {
+            return .primary
+        }
+        
+    }
+}
