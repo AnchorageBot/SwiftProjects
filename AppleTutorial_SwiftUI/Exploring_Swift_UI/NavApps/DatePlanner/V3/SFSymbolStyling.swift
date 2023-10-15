35942bf754ba8664efa4da2985e938f21b1b5186
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-15 11:58:21 -0800

    Create SFSymbolStyling.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/DatePlanner/V3/SFSymbolStyling.swift
@@ -0,0 +1,19 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct SFSymbolStyling: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .imageScale(.large)
+            .symbolRenderingMode(.monochrome)
+    }
+}
+
+extension View {
+    func sfSymbolStyling() -> some View {
+        modifier(SFSymbolStyling())
+    }
+}
