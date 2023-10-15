4051132176f2aadc94c88ecc6b86acc1c29598be
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-15 11:59:03 -0800

    Create SymbolPicker.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/NavApps/DatePlanner/V3/SymbolPicker.swift
@@ -0,0 +1,75 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct SymbolPicker: View {
+    @Binding var event: Event
+    @State private var selectedColor: Color = ColorOptions.default
+    @Environment(\.dismiss) private var dismiss
+    @State private var symbolNames = EventSymbols.symbolNames
+    @State private var searchInput = ""
+    
+    var columns = Array(repeating: GridItem(.flexible()), count: 6)
+
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Text("Done")
+                }
+                .padding()
+            }
+            HStack {
+                Image(systemName: event.symbol)
+                    .font(.title)
+                    .imageScale(.large)
+                    .foregroundColor(selectedColor)
+
+            }
+            .padding()
+
+            HStack {
+                ForEach(ColorOptions.all, id: \.self) { color in
+                    Button {
+                        selectedColor = color
+                        event.color = color
+                    } label: {
+                        Circle()
+                            .foregroundColor(color)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .frame(height: 40)
+
+            Divider()
+
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(symbolNames, id: \.self) { symbolItem in
+                        Button {
+                            event.symbol = symbolItem
+                        } label: {
+                            Image(systemName: symbolItem)
+                                .sfSymbolStyling()
+                                .foregroundColor(selectedColor)
+                                .padding(5)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+                .drawingGroup()
+            }
+        }
+        .onAppear {
+            selectedColor = event.color
+        }
+    }
+}
+
+struct SFSymbolBrowser_Previews: PreviewProvider {
