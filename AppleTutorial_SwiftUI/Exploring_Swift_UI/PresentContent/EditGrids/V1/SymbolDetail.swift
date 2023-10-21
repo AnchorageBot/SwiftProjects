3d3fef8d57a57ed0c444ef22f15e8fa63c00d55b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-20 20:14:47 -0800

    Create SymbolDetail.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/EditGrids/V1/SymbolDetail.swift
@@ -0,0 +1,77 @@
+/*
+
+ SymbolDetail.swift
+ Explore_SwiftUI_V6
+
+ How to create a View file with keyboard and Xcode commands:
+
+    * Command Key (Splat) N -> Swift UI View
+
+ References:
+
+ https://developer.apple.com/tutorials/sample-apps/editinggrids
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/swift/choosing-between-structures-and-classes
+
+ https://developer.apple.com/sf-symbols/
+
+ Created on 10/19/2023 and updated on 10/20/2023 with Swift 5.9, Xcode 15.0
+
+ Creates an image view using an SF Symbol
+
+*/
+
+import SwiftUI
+
+
+struct SymbolDetail: View {
+
+    var symbol: Symbol
+
+
+    var body: some View {
+
+        VStack {
+
+            Text(symbol.name)
+                .font(.system(.largeTitle, design: .rounded))
+
+            Image(systemName: symbol.name)
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.hierarchical)
+                .foregroundColor(.accentColor)
+        }
+        .padding()
+    }
+}
+
+
+/*
+
+Xcode default code, pre October 2023
+
+This works; uncomment it and comment out the #Preview code below if you want to use this code snippet
+
+struct Details_Previews: PreviewProvider {
+    static var previews: some View {
+        SymbolDetail(symbol: Symbol(name: "star"))
+    }
+}
+*/
+
+
+/*
+
+Xcode default code, post October 2023
+
+This works; uncomment it and comment out the struct code if you want to use this code snippet
+
+*/
+
+#Preview {
+    SymbolDetail(symbol: Symbol(name: "star"))
+}
+
