8dcc44b5bc5edffca0d8eed3e039d073574a8dde
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-23 17:33:20 -0800

    Create Explore_SwiftUI_V6app.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/EditGrids/V2/Explore_SwiftUI_V6app.swift
@@ -0,0 +1,59 @@
+/*
+
+ Explore_SwiftUI_V6app.swift
+ Explore_SwiftUI_V6
+
+ How to create this project with Xcode commands:
+
+   * Xcode-> File-> New Project -> iOS - App
+
+ This creates two files
+
+   * ContentView.swift
+   * ProjectName_App.swift
+
+ References:
+
+ https://developer.apple.com/tutorials/sample-apps/organizingwithgrids
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/SwiftUI/App
+
+ https://developer.apple.com/documentation/SwiftUI/App/body-swift.property
+
+ https://developer.apple.com/documentation/SwiftUI/WindowGroup
+
+ https://developer.apple.com/documentation/swift/choosing-between-structures-and-classes
+
+ https://developer.apple.com/documentation/uikit/uitableviewcell/1623229-contentview
+
+ Created on 10/18/2023 and updated on 10/21/2023 with Swift 5.9, Xcode 15.0
+
+ Follows a set of guidelines created by the App protocol. This allows your project to run and behave just like any other app.
+
+*/
+
+import SwiftUI
+
+
+// the entry point of the program — the code the app starts running first
+@main
+
+struct Explore_SwiftUI_V6App: App {
+
+
+    // When your app runs, it displays whatever content you place in the body property
+    var body: some Scene {
+
+        WindowGroup {
+
+            /*
+             When you create a new app, ContentView is configured as your top-level view by default.
+             This is useful because you can go to ContentView to see how an app’s views are put together
+             */
+            ContentView()
+
+        }
+    }
+}
