83460c571bb907b905c61a94203df8eec6c87944
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-23 17:33:56 -0800

    Create ContentView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/EditGrids/V2/ContentView.swift
@@ -0,0 +1,74 @@
+/*
+
+ ContentView.swift
+ Explore_SwiftUI_V6
+
+ How to create this project with Xcode commands:
+
+   * Xcode-> File-> New Project -> iOS - App
+
+ This creates two files
+
+   * ContentView.swift
+   * ProjectName_App.swift
+
+ References:
+
+ https://developer.apple.com/tutorials/sample-apps/editinggrids
+
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/swift/choosing-between-structures-and-classes
+
+ https://developer.apple.com/sf-symbols/
+
+ Created on 10/18/2023 and updated on 10/23/2023 with Swift 5.9, Xcode 15.0
+
+ Creates an image view using an SF Symbol
+
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+
+        VStack {
+
+            /*
+
+             Option One
+
+            SymbolDetail(symbol: Symbol(name: "star"))
+
+            SymbolDetail(symbol: Symbol(name: "brain"))
+
+            SymbolDetail(symbol: Symbol(name: "bicycle"))
+
+            SymbolDetail(symbol: Symbol(name: "map"))
+             */
+
+            
+            /*
+
+             Option Two
+
+            SymbolGrid()
+
+             */
+
+
+            /* Option Three */
+            SymbolPicker(symbol: .constant(nil))
+
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+
+    ContentView()
+}
