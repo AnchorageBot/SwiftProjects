275ba25fc50f16d8195c394f06515873ca12b034
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-29 18:24:24 -0800

    Create Symbol.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/EditGrids/V4/Symbol.swift
@@ -0,0 +1,10 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct Symbol : Identifiable, Equatable {
+    var id = UUID()
+    var name : String
+}
