146d0d8c794397b657c05798faafa8c71a69cf65
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-10-29 18:26:35 -0800

    Create SymbolGridApp.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/EditGrids/V4/SymbolGridApp.swift
@@ -0,0 +1,17 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct SymbolGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                SymbolGrid()
+            }
+        }
+    }
+}
+
