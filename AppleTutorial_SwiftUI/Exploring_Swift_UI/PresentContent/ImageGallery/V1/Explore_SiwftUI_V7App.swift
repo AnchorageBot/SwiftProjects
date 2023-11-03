3257b79c3d5ee821fca6d20ba20d80c3db1d283e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-11-02 19:15:56 -0800

    Update Explore_SiwftUI_V7App.swift

--- AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V1/Explore_SiwftUI_V7App.swift
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V1/Explore_SiwftUI_V7App.swift
@@ -1,3 +1,21 @@
+/*
+
+ Explore_SiwftUI_V7App.swift
+ Explore_SwiftUI_V7
+
+ How to create this project with Xcode commands:
+
+   * Xcode-> File-> New Project -> iOS - App
+
+ This creates two files
+
+   * ContentView.swift
+   * ProjectName_App.swift
+
+ References:
+
+ https://developer.apple.com/tutorials/sample-apps/imagegallery
+
  https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
 
  https://developer.apple.com/documentation/SwiftUI/App
@@ -9,3 +27,23 @@
  https://developer.apple.com/documentation/swift/choosing-between-structures-and-classes
 
  https://developer.apple.com/documentation/uikit/uitableviewcell/1623229-contentview
+
+ Created on 11/2/2023 with Swift 5.9, Xcode 15.0
+
+*/
+
+import SwiftUI
+
+@main
+
+struct Explore_SwiftUI_V7App: App {
+
+    var body: some Scene {
+
+        WindowGroup {
+
+            ContentView()
+
+        }
+    }
+}
