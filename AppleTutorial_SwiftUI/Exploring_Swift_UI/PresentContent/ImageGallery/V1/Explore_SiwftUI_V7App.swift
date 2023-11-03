ca9c7712f878e5f1c589adce0ea7af344413e886
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-11-02 19:15:38 -0800

    Create Explore_SiwftUI_V7App.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V1/Explore_SiwftUI_V7App.swift
@@ -0,0 +1,11 @@
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
+
+ https://developer.apple.com/documentation/SwiftUI/App
+
+ https://developer.apple.com/documentation/SwiftUI/App/body-swift.property
+
+ https://developer.apple.com/documentation/SwiftUI/WindowGroup
+
+ https://developer.apple.com/documentation/swift/choosing-between-structures-and-classes
+
+ https://developer.apple.com/documentation/uikit/uitableviewcell/1623229-contentview
