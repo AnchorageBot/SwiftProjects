0822606de0d5ef23ccda3d569c63dec3f18d3e55
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-11-04 15:02:53 -0800

    Update Item.swift

--- AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V1/Item.swift
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V1/Item.swift
@@ -17,14 +17,28 @@
 
  https://developer.apple.com/documentation/Swift/Identifiable
 
+ https://developer.apple.com/documentation/foundation/url
+
+ https://developer.apple.com/documentation/foundation/uuid
+
  https://docs.swift.org/swift-book/documentation/the-swift-programming-language/extensions/
 
- Created on 11/2/2023 with Swift 5.9, Xcode 15.0
+ Created on 11/2/2023 and updated on 11/4/2023 with Swift 5.9, Xcode 15.0
+
+ Models a grid item’s data and adds Identifiable conformance
 
 */
 
 import SwiftUI
 
+
+/*
+
+ When you use ForEach to construct a collection view, such as a list or grid, SwiftUI wants to uniquely identify each view
+ so it can find and update them. To make this easier, you can create a model that conforms to Identifiable, which creates an
+ id property requirement.
+
+ */
 
 struct Item: Identifiable {
 
@@ -44,5 +58,3 @@ extension Item: Equatable {
         return lhs.id == rhs.id && lhs.id == rhs.id
     }
 }
-
-
