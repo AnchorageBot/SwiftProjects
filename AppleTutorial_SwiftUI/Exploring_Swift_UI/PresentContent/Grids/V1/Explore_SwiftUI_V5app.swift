80ceb52aa1fbffeac5d30d40e272b6a748a2492b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-11-04 15:06:24 -0800

    Update Explore_SwiftUI_V5app.swift

--- AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/Grids/V1/Explore_SwiftUI_V5app.swift
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/Grids/V1/Explore_SwiftUI_V5app.swift
@@ -1,7 +1,7 @@
 /*
 
- Explore_SwiftUI_V5app.swift
- Explore_SwiftUI_V5
+ Explore_SiwftUI_V7App.swift
+ Explore_SwiftUI_V7
 
  How to create this project with Xcode commands:
 
@@ -14,7 +14,7 @@
 
  References:
 
- https://developer.apple.com/tutorials/sample-apps/organizingwithgrids
+ https://developer.apple.com/tutorials/sample-apps/imagegallery
 
  https://docs.swift.org/swift-book/documentation/the-swift-programming-language/
 
@@ -28,7 +28,7 @@
 
  https://developer.apple.com/documentation/uikit/uitableviewcell/1623229-contentview
 
- Created on 10/16/2023 and updated on 10/11/2023 with Swift 5.9, Xcode 15.0
+ Created on 11/2/2023 and updated on 11/4/2023 with Swift 5.9, Xcode 15.0
 
  Follows a set of guidelines created by the App protocol. This allows your project to run and behave just like any other app.
 
@@ -36,25 +36,16 @@
 
 import SwiftUI
 
-
-// the entry point of the program — the code the app starts running first
 @main
 
-struct Explore_SwiftUI_V5App: App {
+struct Explore_SwiftUI_V7App: App {
 
-
-    // When your app runs, it displays whatever content you place in the body property
     var body: some Scene {
 
         WindowGroup {
 
-            /*
-             When you create a new app, ContentView is configured as your top-level view by
-             default. This is useful because you can go to ContentView to see how an app’s
-             views are put together
-             */
             ContentView()
-
+            
         }
     }
 }
