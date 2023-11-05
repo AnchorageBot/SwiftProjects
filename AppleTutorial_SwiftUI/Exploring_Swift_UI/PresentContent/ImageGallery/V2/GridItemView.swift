9b420c09d72c7663dc403de2c389d4d2569348f5
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-11-04 16:46:34 -0800

    Create GridItemView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V2/GridItemView.swift
@@ -0,0 +1,31 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct GridItemView: View {
+    let size: Double
+    let item: Item
+
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            AsyncImage(url: item.url) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: size, height: size)
+        }
+    }
+}
+
+struct GridItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        if let url = Bundle.main.url(forResource: "mushy1", withExtension: "jpg") {
+            GridItemView(size: 50, item: Item(url: url))
+        }
+    }
+}
