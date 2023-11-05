2c816cffbac1b55a3cb809648a37adba326b6af2
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-11-04 16:48:01 -0800

    Create Item.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V2/Item.swift
@@ -0,0 +1,18 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct Item: Identifiable {
+
+    let id = UUID()
+    let url: URL
+
+}
+
+extension Item: Equatable {
+    static func ==(lhs: Item, rhs: Item) -> Bool {
+        return lhs.id == rhs.id && lhs.id == rhs.id
+    }
+}
