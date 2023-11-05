5e6bd9029e7556ae95139c8244ad661f681ec2ea
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-11-04 16:43:19 -0800

    Create DetailView.swift

---
+++ AppleTutorial_SwiftUI/Exploring_Swift_UI/PresentContent/ImageGallery/V2/DetailView.swift
@@ -0,0 +1,27 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct DetailView: View {
+    let item: Item
+
+    var body: some View {
+        AsyncImage(url: item.url) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
+        }
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        if let url = Bundle.main.url(forResource: "mushy1", withExtension: "jpg") {
+            DetailView(item: Item(url: url))
+        }
+    }
+}
