bd31e20cc6ef5064b65d4c0b77898fbac3a212c8
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-12-13 10:52:18 -0700

    Create HorizontalFlip.swift

---
+++ AppleTutorial_SwiftData/WWDC203_SwiftDataApp/V1/HorizontalFlip.swift
@@ -0,0 +1,24 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view modifier for a flip visual effect.
+*/
+
+import SwiftUI
+
+struct HorizontalFlip: ViewModifier {
+    var angle: Angle
+    var visible: Bool
+    func body(content: Content) -> some View {
+        content
+            .rotation3DEffect(angle, axis: (x: 0.0, y: 1.0, z: 0.0))
+            .opacity(visible ? 1.0 : 0.0)
+    }
+}
+
+extension View {
+    func horizontalFlip(_ angle: Angle, visible: Bool) -> some View {
+        modifier(HorizontalFlip(angle: angle, visible: visible))
+    }
+}
