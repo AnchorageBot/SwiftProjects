e665756f9c7bef29adedb6aad8c39c3f417f3cde
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-12-13 14:38:09 -0700

    Create UTType+FlashCards.swift

---
+++ AppleTutorial_SwiftData/WWDC203_SwiftDataApp/V1/UTType+FlashCards.swift
@@ -0,0 +1,12 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The UTType for the document representing a deck of cards.
+*/
+
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var flashCards = UTType(exportedAs: "com.example.flashCards")
+}
