f963cb003517f6c3af901961434d9ba74deb7a5c
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-12-16 15:45:40 -0900

    Create M_SwiftDataFlashCardSample.swift

---
+++ AppleTutorial_SwiftData/WWDC203_SwiftDataApp/V2/M_SwiftDataFlashCardSample.swift
@@ -0,0 +1,17 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The main entry for the app.
+*/
+
+import SwiftUI
+
+@main
+struct SwiftDataFlashCardSample: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
