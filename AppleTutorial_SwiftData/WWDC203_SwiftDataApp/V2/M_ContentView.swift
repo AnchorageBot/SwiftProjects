8c1583eaf15d9358dd13d3a6d141e62000e80c0d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-12-17 15:03:17 -0900

    Update M_ContentView.swift

--- AppleTutorial_SwiftData/WWDC203_SwiftDataApp/V2/M_ContentView.swift
+++ AppleTutorial_SwiftData/WWDC203_SwiftDataApp/V2/M_ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @Query private var cards: [Card] 
     @State private var editing = false
     @State private var navigationPath: [Card] = []
+    @Environment(\.modelContext) private var modelContext
 
     var body: some View {
         NavigationStack(path: $navigationPath) {
