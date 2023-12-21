d77e13338f8887e67ddf59f115e52c3c6fe4bc10
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-12-20 19:20:45 -0900

    Create M_SwiftDataFlashCardSample.swift

---
+++ AppleTutorial_SwiftData/SwiftDataApp/V2/M_SwiftDataFlashCardSample.swift
@@ -0,0 +1,24 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The main entry for the app.
+*/
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftDataFlashCardSample: App {
+    var body: some Scene {
+        #if os(iOS) || os(macOS)
+        DocumentGroup(editing: Card.self, contentType: .flashCards) {
+            ContentView()
+        #else
+        WindowGroup {
+            ContentView()
+                .modelContainer(for: Card.self)
+        }
+        #endif
+    }
+}
