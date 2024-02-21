8549d4cb76e103cd18675e85515d59bb18d8c4a3
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-02-20 21:21:43 -0900

    Create App_VstackApp.swift

---
+++ Udemy_SwiftUIprojects/Vstack/App_VstackApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_VstackApp.swift
+//  App_Vstack
+//
+//  Created by ATS on 2/20/24
+//
+
+import SwiftUI
+
+@main
+struct App_VstackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
