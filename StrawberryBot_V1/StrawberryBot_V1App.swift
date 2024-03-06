a15a3be9b32f2fed7098a25b2d5fe58467ea3974
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-06 05:14:57 -0900

    Create StrawberryBot_V1App.swift

---
+++ StrawberryBot_V1/StrawberryBot_V1App.swift
@@ -0,0 +1,32 @@
+//
+//  Main Group
+//  StrawberryBot_V1App.swift
+//  StrawberryBot_V1
+//
+//  Created by ATS on 3/5/24
+//  Xcode version 15.2
+//  Last revised on 
+
+/*
+ Abstract:
+ 
+ The main entry for the app
+ 
+ The app allows a user to take a photo of a plant, store the photo, have the photo analyzed for gardening suggestions, purchase another photo analysis, check garden weather, and check a garden location map
+ 
+ References:
+ 
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+ 
+ */
+
+import SwiftUI
+
+@main
+struct StrawberryBot_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
