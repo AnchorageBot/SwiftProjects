9e1c54f96d254d6e63720284e6ad913da98a8282
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-06 05:17:28 -0900

    Create CheckupView.swift

---
+++ StrawberryBot_V1/CheckupView.swift
@@ -0,0 +1,48 @@
+//
+//  AppViews Group
+//  CheckupView.swift
+//  StrawberryBot_V1
+//
+//  Created by ATS on 3/5/24
+//  Xcode version 15.2
+//  Last revised on 
+
+/*
+ Abstract:
+ 
+ The checkup view shows the most recent analysis of the plant photograph received from Anthropic Claude AI and allows the user to purchase another analysis
+ 
+ References:
+ 
+ https://developer.apple.com/tutorials/Sample-Apps
+ 
+ */
+
+import SwiftUI
+
+struct CheckupView: View {
+    var body: some View {
+        
+        Image(systemName: "stethoscope")
+            .imageScale(.large)
+            .foregroundColor(Color.blue)
+            .foregroundStyle(.tint)
+        
+            .padding()
+        
+        Text("Plant Checkup Results!")
+        
+        Image(systemName: "person")
+            .imageScale(.large)
+            .foregroundColor(Color.blue)
+            .foregroundStyle(.tint)
+        
+            .padding()
+        
+        Text("Buy Another Plant Checkup!")
+    }
+}
+
+#Preview {
+    CheckupView()
+}
