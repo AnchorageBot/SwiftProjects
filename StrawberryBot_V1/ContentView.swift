d468dac4c6ae963ce77eb69dfb13c61d569055a2
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-06 05:15:45 -0900

    Create ContentView.swift

---
+++ StrawberryBot_V1/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  Main Group
+//  ContentView.swift
+//  StrawberryBot_V1
+//
+//  Created by ATS on 3/5/24
+//  Xcode version 15.2
+//  Last revised on 
+
+/*
+ Abstract:
+ 
+ The main view that contains the majority of the app's content
+ 
+ The content view is the welocme mat that takes the user to the navigation view
+ 
+ References:
+ 
+ https://developer.apple.com/tutorials/swiftui-concepts/exploring-the-structure-of-a-swiftui-app
+ 
+ https://developer.apple.com/tutorials/swiftui-concepts/maintaining-the-adaptable-sizes-of-built-in-views
+ 
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Image(systemName: "leaf")
+                .imageScale(.large)
+                .foregroundColor(Color.green)
+                .foregroundStyle(.tint)
+            
+                .padding()
+            
+            Button("Let's Garden!") {
+                NavigationLink {
+                    NavigationView()
+                } label: {
+                    NavigationView()
+                }
+
+            }
+            
+                .padding()
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
