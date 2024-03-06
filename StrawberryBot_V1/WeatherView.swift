d2923666d29271208a3f39db128bacd65f41ddd2
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-06 05:17:59 -0900

    Create WeatherView.swift

---
+++ StrawberryBot_V1/WeatherView.swift
@@ -0,0 +1,57 @@
+//
+//  AppViews Group
+//  WeatherView.swift
+//  StrawberryBot_V1
+//
+//  Created by ATS on 3/5/24
+//  Xcode version 15.2
+//  Last revised on 
+
+/*
+ Abstract:
+ 
+ The weather view shows the garden's weather
+ 
+ References:
+ 
+ https://developer.apple.com/tutorials/Sample-Apps
+ 
+ */
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        
+        Image(systemName: "cloud")
+            .imageScale(.large)
+            .foregroundColor(Color.blue)
+            .foregroundStyle(.tint)
+        
+            .padding()
+        
+        Text("Hello, Garden Weather!")
+        
+            .padding()
+        
+        /*
+         
+         // Failed attempt to link back to ContentView screen
+         
+         TabView {
+         
+         NavigaitionView()
+         .tabItem {
+         Label("Home", systemImage: "home")
+         }
+         }
+         
+        */
+    }
+}
+
+#Preview {
+    WeatherView()
+}
+
+
