3dc7b7a89e439d22416bba2f7b821e8ce8e4e8d7
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-09 17:56:33 -0900

    Create CoffeeOrderAPIAPP.swift

---
+++ Udemy_SwiftUIprojects/API_Calls/CoffeeOrderAPIAPP.swift
@@ -0,0 +1,25 @@
+//
+//  Group Main
+//  CoffeeOrderAPIApp.swift
+//  CoffeeOrderAPI
+//
+//  Created by ATS on 3/8/24
+//
+
+/*
+ Abstract:
+ 
+ Reference:
+ Livestream - Learn how to make an API call in SwiftUI - azamsharp - YouTube
+ */
+
+import SwiftUI
+
+@main
+struct CoffeeOrderAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
