624de6f48f917df33cd5e2d0333b94039dfb926b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-09 18:01:36 -0900

    Create Order.swift

---
+++ Udemy_SwiftUIprojects/API_Calls/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Group Models
+//  Order.swift
+//  CoffeeOrderAPI
+//
+//  Created by ATS on 3/8/24
+//
+
+/*
+ Abstract:
+ 
+ References:
+ Livestream - Learn how to make an API call in SwiftUI - azamsharp - YouTube
+ 
+ https://developer.apple.com/documentation/swiftui/environmentvalues/authorizationcontroller
+ */
+
+import Foundation
+
+
+struct Order: Codeable {
+    //var id: , Int?
+    //let name: "Java Joe", String
+    //let coffeeName: "Americano", String
+    //let total: , Double
+    //let size: "Large", String
+}
