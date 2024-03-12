4ef5d7d6cedd7b2915434091d42627c99c45d843
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-11 22:24:54 -0800

    Create WxCoreLocate_V1App.swift

---
+++ YouTube_SwiftTutorials/Weather_SwiftUI/WxCoreLocate_V1App.swift
@@ -0,0 +1,25 @@
+//
+//  WxCoreLocate_V1App.swift
+//  WxCoreLocate_V1
+//
+//  Created by ATS on 3/11/24
+//
+
+/*
+ 
+ Abstract:
+ 
+ References:
+ 
+ */
+
+import SwiftUI
+
+@main
+struct WxCoreLocate_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
