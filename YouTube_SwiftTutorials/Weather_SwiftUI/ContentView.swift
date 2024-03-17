71ecd5d2cc491ed8957748fe16bbb097bb76f673
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-16 21:50:47 -0800

    Update ContentView.swift

--- YouTube_SwiftTutorials/Weather_SwiftUI/ContentView.swift
+++ YouTube_SwiftTutorials/Weather_SwiftUI/ContentView.swift
@@ -3,18 +3,27 @@
 //  WxCoreLocate_V1
 //
 //  Created by ATS on 3/11/24
+//  Revised on 3/16/24
+//  Xcode 15.2
 //
 
 /*
  
  Abstract:
  
+    A view controller manages a view hierarchy and the state information needed to keep those views up-to-date
+ 
  References:
+ 
+    https://developer.apple.com/documentation/uikit/view_controllers/displaying_and_managing_views_with_a_view_controller
+ 
+    https://developer.apple.com/documentation/uikit/view_controllers/managing_content_in_your_app_s_windows/
  
  */
 
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
     var body: some View {
