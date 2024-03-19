30c49c0ee0ed98a084a2278e0a30074b6ce705d4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-18 18:29:06 -0800

    Update ContentView.swift

--- YouTube_SwiftTutorials/Weather_SwiftUI/V1/ContentView.swift
+++ YouTube_SwiftTutorials/Weather_SwiftUI/V1/ContentView.swift
@@ -1,9 +1,10 @@
 //
+//  Main Group
 //  ContentView.swift
 //  WxCoreLocate_V1
 //
 //  Created by ATS on 3/11/24
-//  Revised on 3/17/24
+//  Revised on 3/18/24
 //  Xcode 15.2
 //
 
@@ -11,16 +12,20 @@
  
  Abstract:
  
-    A view controller manages a view hierarchy and the state information needed to keep those views up-to-date
+    The primary role of a content view is to display the app’s data. A view controller manages a view hierarchy and the state information needed to keep those views up-to-date
  
  References:
  
     Build a Weather App with SwiftUI | JSON API & CoreLocation Tutorial
     www.youtube.com/@Cebraiil
  
+    https://developer.apple.com/tutorials/app-dev-training/managing-content-views/
+ 
     https://developer.apple.com/documentation/uikit/view_controllers/displaying_and_managing_views_with_a_view_controller
  
     https://developer.apple.com/documentation/uikit/view_controllers/managing_content_in_your_app_s_windows/
+ 
+    https://developer.apple.com/documentation/uikit/uicollectionviewcell/1620133-contentview/
  
     https://www.meteomatics.com
  
@@ -54,7 +59,7 @@ struct ContentView: View {
                 Text("CodeLab")
                     .bold()
                     .padding()
-                    .foregroundColor(.blue)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
             } else {
                 ProgressView()
             }
@@ -78,8 +83,19 @@ struct ContentView: View {
         
         URLSession.shared.dataTask(with: url) { data, _ , error in guard let data = data else {return}
             
-            // 6, 7
-        }
+            do {
+                let decoder = JSONDecoder()
+                let weatherResponse = try decoder.decode(WeatherResponse.self, from: data)
+                
+                DispatchQueue.main.async {
+                    weatherData = weatherData(locationName: weatherResponse.name, temperature:
+                        weatherResponse.main.temp, condition:
+                        weatherResponse.weather.first?.description ?? "")
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
     
 }
