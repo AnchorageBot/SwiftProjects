5937f613e2c53181ad15c2ff9ad0ba881589176d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-18 18:27:21 -0800

    Update WxCoreLocate_V1App.swift

--- YouTube_SwiftTutorials/Weather_SwiftUI/V1/WxCoreLocate_V1App.swift
+++ YouTube_SwiftTutorials/Weather_SwiftUI/V1/WxCoreLocate_V1App.swift
@@ -1,15 +1,26 @@
 //
+//  Main Group
 //  WxCoreLocate_V1App.swift
 //  WxCoreLocate_V1
 //
 //  Created by ATS on 3/11/24
+//  Revised on 3/18/24
+//  Xcode 15.2
 //
 
 /*
  
  Abstract:
  
+    Manages the app launch process
+ 
  References:
+ 
+    https://developer.apple.com/documentation/uikit/uiapplicationdelegate/3656306-main
+ 
+    https://developer.apple.com/documentation/swiftui/app/main()/
+ 
+    https://developer.apple.com/documentation/swiftui/app
  
  */
 
