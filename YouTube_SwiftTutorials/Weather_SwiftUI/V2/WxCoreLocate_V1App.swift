cfbad650118a1bac4ed1673a0d10198e2238ee62
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-18 18:33:52 -0800

    Create  WxCoreLocate_V1App.swift

---
+++ YouTube_SwiftTutorials/Weather_SwiftUI/V2/WxCoreLocate_V1App.swift
@@ -0,0 +1,35 @@
+//
+//  Main Group
+//  WxCoreLocate_V1App.swift
+//  WxCoreLocate_V2
+//
+//  Created by Claude and ATS on 3/18/24
+//  Xcode 15.2
+//
+
+/*
+ 
+ Abstract:
+ 
+    Manages the app launch process
+ 
+ References:
+ 
+    https://developer.apple.com/documentation/uikit/uiapplicationdelegate/3656306-main
+ 
+    https://developer.apple.com/documentation/swiftui/app/main()/
+ 
+    https://developer.apple.com/documentation/swiftui/app
+ 
+ */
+
+import SwiftUI
+
+@main
+struct WxCoreLocate_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
