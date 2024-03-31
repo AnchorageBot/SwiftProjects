7f5dee5899b394400730c495d20b00ee03c39e18
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-03-31 15:16:24 -0800

    Create ImmersiveView.swift

---
+++ YouTube_SwiftTutorials/VisionOS_Snow/V1/ImmersiveView.swift
@@ -0,0 +1,26 @@
+//
+//  ImmersiveView.swift
+//  VOS_SnowV1
+//
+//  Created by ATS on 3/31/24
+//  Mode with Xcode 15.3
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+}
