bb5cfce270798929979e266ffd1bd2d318b65fff
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-04 20:01:17 -0800

    Create ChatPrototypeApp.swift

---
+++ AppleTutorial_DevelopSwift/Foundations/ExploreXcode/ChatPrototypeApp.swift
@@ -0,0 +1,21 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by ATS on 4/4/24
+//  Made with 15.3
+//
+
+import SwiftUI
+
+@main
+
+struct ChatPrototypeApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
