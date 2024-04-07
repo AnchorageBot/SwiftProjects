8194ef5b46aecffa686d2d8d2751b120acf719c6
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-07 01:59:10 -0800

    Create ContentView.swift

---
+++ AppleTutorial_DevelopSwift/Foundations/ViewsStructures/V2/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  WeatherPrototype
+//
+//  Created by ATS on 4/6/24
+//  Made with Xcode 15.3
+//  Revised on 4/7/24
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        HStack {
+            
+            DayForecast(day: "Mon", seeSun: true, high: 60, low: 50)
+            
+            DayForecast(day: "Tue", seeSun: false, high: 50, low: 40)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct DayForecast: View {
+    
+    let day: String
+    let seeSun: Bool
+    let high: Int
+    let low: Int
+    
+    var WxEmojiName: String {
+        if seeSun {
+            return "sun.max.fill"
+        } else {
+            return "cloud.snow.fill"
+        }
+    }
+    
+    var WxEmojiColor: Color {
+        if seeSun {
+            return Color.yellow
+                
+        } else {
+            return Color.blue
+        }
+    }
+    
+    var body: some View {
+        
+        VStack {
+            
+            Text(day)
+            
+            Image(systemName: WxEmojiName)
+                .foregroundColor(WxEmojiColor)
+                //.foregroundStyle(WxEmojiColor)
+                .padding()
+            
+            // to display the value of a number in a text view, which expects a String, you have to change the number into a string
+            Text("High: \(high)")
+            Text("Low: \(low)")
+            
+        }
+        .padding()
+    }
+}
