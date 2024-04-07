6e02ba63b755eeca9cbaf1c18c1832d8b34a04f7
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-07 02:00:09 -0800

    Create WeatherPrototypeApp.swift

---
+++ AppleTutorial_DevelopSwift/Foundations/ViewsStructures/V2/WeatherPrototypeApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherPrototypeApp.swift
+//  WeatherPrototype
+//
+//  Created by ATS on 4/6/24
+//  Made with Xcode 15.3
+//
+
+import SwiftUI
+
+@main
+struct WeatherPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
