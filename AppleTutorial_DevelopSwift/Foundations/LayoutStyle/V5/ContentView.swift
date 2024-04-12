e27e47a1640f36625ff306691162100b512ed34e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-11 21:48:58 -0800

    Create ContentView.swift

---
+++ AppleTutorial_DevelopSwift/Foundations/LayoutStyle/V5/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  OnboardingFlow
+//
+//  Created by ATS on 4/7/24
+//  Made with Xcode 15.3
+//  Revised on 4/11/24
+//
+//  Chapter 7 of the tutorial
+
+/*
+Abstract:
+The primary role of a content view is to display the app's data.
+This code represents the app's main content view. The view is built using SwiftUI, which allows for a declarative and concise way of constructing the user interface.
+
+References:
+- SwiftUI App Life Cycle: https://developer.apple.com/documentation/swiftui/app
+- SwiftUI App Protocol: https://developer.apple.com/documentation/swiftui/app
+- SwiftUI WindowGroup: https://developer.apple.com/documentation/swiftui/windowgroup
+- Develop in Swift Tutorials - Layout and style
+    https://developer.apple.com/tutorials/develop-in-swift/design-an-interface
+- Anthropic AI Claude
+     https://www.anthropic.com
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    // gradientTop and gradientBottom RGB values are created and defined in Assets
+    let gradientColors: [Color] = [
+        .gradientTop,
+        .gradientBottom
+    ]
+    
+    var body: some View {
+        
+        TabView {
+            
+            WelcomePage()
+            
+            FeaturesPage()
+            
+        }
+        .background(Gradient(colors: gradientColors))
+        .tabViewStyle(.page)
+    }
+}
+
+#Preview {
+    ContentView()
+}
