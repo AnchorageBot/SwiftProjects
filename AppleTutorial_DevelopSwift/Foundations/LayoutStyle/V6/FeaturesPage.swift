64ca0148c4fad2893118d5cf385d79b1358a917f
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-13 11:01:02 -0800

    Create FeaturesPage.swift

---
+++ AppleTutorial_DevelopSwift/Foundations/LayoutStyle/V6/FeaturesPage.swift
@@ -0,0 +1,58 @@
+//
+//  FeaturesPage.swift
+//  OnboardingFlow
+//
+//  Created by ATS on 4/9/24
+//  Made with Xcode 15.3
+//  Revised on 4/13/24
+//
+//  Chapter 8 of the tutorial
+
+/*
+Abstract:
+ This code defines the FeaturesPage struct, which displays a list of cards describing the app’s features. The view is built using SwiftUI, which allows for a declarative and concise way of constructing the user interface.
+
+References:
+- SwiftUI App Life Cycle: https://developer.apple.com/documentation/swiftui/app
+- SwiftUI App Protocol: https://developer.apple.com/documentation/swiftui/app
+- SwiftUI WindowGroup: https://developer.apple.com/documentation/swiftui/windowgroup
+ - Apple Developer https://developer.apple.com/documentation/
+- Develop in Swift Tutorials - Layout and style
+    https://developer.apple.com/tutorials/develop-in-swift/design-an-interface
+- Anthropic AI Claude
+     https://www.anthropic.com
+*/
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    
+    // gradientTop and gradientBottom RGB values are created and defined in Assets
+    //let gradientColors: [Color] = [
+        //.gradientTop,
+        //.gradientBottom
+    //]
+    
+    var body: some View {
+        
+        VStack {
+        
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding()
+            
+            FeaturesCard(iconName: "person.2.crop.square.stack.fill", description: "Description and image")
+       
+            FeaturesCard(iconName:"quote.bubble.fill", description: "Short summary")
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FeaturesPage()
+        //.background(Gradient(colors: gradientColors))
+        .foregroundStyle(.white)
+}
