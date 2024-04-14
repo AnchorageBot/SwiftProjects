d3f4eaf5a77a6c69904532465732f40f6d330db4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-14 11:12:10 -0800

    Create ContentView.swift

---
+++ AppleTutorial_DevelopSwift/Foundations/ButtonsState/V1/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  DiceView
+//
+//  Created by ATS on 4/14/24
+//  Xcode version 15.3
+//
+//  Chapter 1 of the tutorial
+//
+
+/*
+ 
+ Abstract:
+ The primary role of a content view is to display the app's data.
+ This code represents the app's main content view. The view is built using SwiftUI, which allows for a declarative and concise way of constructing the user interface.
+ References:
+ - Update the UI with state https://developer.apple.com/tutorials/develop-in-swift/update-the-ui-with-state
+ 
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        HStack {
+            
+            DiceView()
+            DiceView()
+            DiceView()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
