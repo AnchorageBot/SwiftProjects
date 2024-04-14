9aea81615062eb94ba69396fd114e320b5b7f584
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-13 17:09:58 -0800

    Create OnboardingFlowApp.swift

---
+++ AppleTutorial_DevelopSwift/Foundations/LayoutStyle/V9/OnboardingFlowApp.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by ATS on 4/7/24
+//  Made with Xcode 15.3
+//
+//  Refactored and commented by Claude on 4/13/24
+//
+
+import SwiftUI
+
+/// The main entry point of the OnboardingFlow app.
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            /// The initial view of the app.
+            ContentView()
+        }
+    }
+}
