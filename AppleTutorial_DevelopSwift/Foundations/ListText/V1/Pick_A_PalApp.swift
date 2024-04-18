e8c1f9d9bd4764165aecc9b4aeb75f3e802fc671
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-17 20:11:39 -0800

    Update Pick_A_PalApp.swift

--- AppleTutorial_DevelopSwift/Foundations/ListText/V1/Pick_A_PalApp.swift
+++ AppleTutorial_DevelopSwift/Foundations/ListText/V1/Pick_A_PalApp.swift
@@ -23,14 +23,18 @@ import SwiftUI
 
 // The @main attribute indicates that this is the entry point of the application.
 @main
+
 // Structures (struct) are used to represent common kinds of data
 // They can include stored properties, computed properties, and methods. Moreover, Swift structures can adopt protocols to gain behavior through default implementations.
 struct Pick_A_PalApp: App {
+ 
     // The body property is a computed property that returns the app's main scene.
     var body: some Scene {
+     
         // WindowGroup is a scene that represents the main window of the app.
         // It contains the initial view hierarchy of the app.
         WindowGroup {
+         
             // ContentView() is the initial view that will be displayed when the app launches.
             // The actual content and layout of the ContentView will be defined in a separate file.
             ContentView()
