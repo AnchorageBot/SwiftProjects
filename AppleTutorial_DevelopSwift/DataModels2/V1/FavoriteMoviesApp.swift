28f6407839035b79d0a212db92454e5447692759
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-23 18:48:55 -0800

    Create FavoriteMoviesApp.swift

---
+++ AppleTutorial_DevelopSwift/DataModels2/V1/FavoriteMoviesApp.swift
@@ -0,0 +1,32 @@
+//
+//  FavoriteMoviesApp.swift
+//  FavoriteMovies
+//
+//  Created by ATS on 4/23/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FavoriteMoviesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
