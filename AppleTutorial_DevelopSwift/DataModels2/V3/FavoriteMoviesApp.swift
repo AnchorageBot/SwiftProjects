010a2f97ab253eb4e12c2c91a99131e3ad6446b7
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-04-25 19:47:09 -0800

    Create FavoriteMoviesApp.swift

---
+++ AppleTutorial_DevelopSwift/DataModels2/V3/FavoriteMoviesApp.swift
@@ -0,0 +1,52 @@
+//
+//  FavoriteMoviesApp.swift
+//  FavoriteMovies
+//
+//  Created by ATS on 4/23/24
+//  Made with Xcode 15.3
+//  Commented and refactored by Claude on 4/25/24
+//
+
+/*
+Abstract:
+This code serves as the entry point of the FavoriteMovies app. It sets up the app's
+main structure using the App protocol and specifies the initial view to be
+displayed when the app launches. The ContentView will contain the main content
+and layout of the app, which is defined in a separate file.
+
+References:
+- App Protocol Documentation
+  https://developer.apple.com/documentation/swiftui/app
+- SwiftData Documentation
+  https://developer.apple.com/documentation/swiftdata
+*/
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FavoriteMoviesApp: App {
+    // MARK: - Properties
+    
+    /// The shared model container that manages the app's data.
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Movie.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    // MARK: - App Body
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
