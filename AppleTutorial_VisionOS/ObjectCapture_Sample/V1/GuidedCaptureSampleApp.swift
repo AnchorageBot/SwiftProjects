06b7a779be889405b99d3dde85ef21678da11f06
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-05-25 14:57:40 -0800

    Add files via upload

---
+++ AppleTutorial_VisionOS/ObjectCapture_Sample/V1/GuidedCaptureSampleApp.swift
@@ -0,0 +1,21 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+Top-level app structure of the view hierarchy.
+*/
+
+import SwiftUI
+
+@main
+struct GuidedCaptureSampleApp: App {
+    static let subsystem: String = "com.example.apple-samplecode.GuidedCapture"
+    
+    var body: some Scene {
+        WindowGroup {
+            if #available(iOS 17.0, *) {
+                ContentView()
+            }
+        }
+    }
+}
