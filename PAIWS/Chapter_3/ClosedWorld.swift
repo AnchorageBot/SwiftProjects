24cae068d05ce47da0d47cf85a33a488cf07e553
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-06-24 19:27:17 -0800

    Update ClosedWorld.swift

--- PAIWS/Chapter_3/ClosedWorld.swift
+++ PAIWS/Chapter_3/ClosedWorld.swift
@@ -1,1 +1,20 @@
+/*
+
+Practical Artificial Intelligence with Swift
+
+Closed-World Assumptions
+This term describes inaccurate assumptions that the information possessed about a thiing
+encapsulates or is indicative of the whole
+
+*/
+
+let noResponses = ["N", "n", "no", "No"]
+
+if yesResponses.contains(input) {
+    print("Answer was yes.")
+} else if noResponses.contains(input) {
+    print("Answer was no.")
+} else {
+    print("Answer was not yes or no.")
+}
 
