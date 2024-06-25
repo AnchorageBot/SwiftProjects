139e06477ea485a7c369d5380a0bf54788e3f29b
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-06-24 19:22:56 -0800

    Create NegationFailure.swift

---
+++ PAIWS/Chapter_3/NegationFailure.swift
@@ -0,0 +1,15 @@
+/*
+
+Practical Artificial Intelligence with Swift
+
+Negation Failure Example
+This term is used to describe the problems that can arise with defaulting to negative where positive cannot be proven.
+
+*/
+
+if input == "Y" {
+    print("Answer was yes.")
+} else {
+    print("Answer was no.")
+}
+
