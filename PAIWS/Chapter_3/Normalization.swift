126eb128847e4a35dbfcc9ec3fd530b827c3251e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-06-24 19:42:03 -0800

    Update Normalization.swift

--- PAIWS/Chapter_3/Normalization.swift
+++ PAIWS/Chapter_3/Normalization.swift
@@ -1,1 +1,22 @@
+/*
+
+Practical Artificial Intelligence with Swift
+
+Normalization
+A method applied to numerical data to bind the upper and lower bounds to a scale that will 
+make them easier to work with
+
+*/
+
+func normalise(_ value: Double, upperBound: Double) -> Double {
+        return (value / upperBound) * 1.0
+    }
+
+    // function normalizes only positive values
+
+    length = normalise(length, upperBound: maxLength)
+    weight = normalise(weight, upperBound: maxWeight)
+    age = normalise(age, upperBound: theoreticalAgeLimit)
+    eyesDifferent = normalise(eyesDifferent, upperBound: 2)
+
 
