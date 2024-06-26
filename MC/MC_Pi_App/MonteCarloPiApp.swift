e25687efbb8a191c8b59492fb6b585f8e4761327
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-06-25 19:35:51 -0800

    Create MonteCarloPiApp.swift

---
+++ MC/MC_Pi_App/MonteCarloPiApp.swift
@@ -0,0 +1,18 @@
+//
+//  MonteCarloPiApp.swift
+//  MonteCarloPi
+//
+//  Created by ATS and Claude on 6/25/24
+//  Made with Xcode 15.4
+//
+
+import SwiftUI
+
+@main
+struct MonteCarloPiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
