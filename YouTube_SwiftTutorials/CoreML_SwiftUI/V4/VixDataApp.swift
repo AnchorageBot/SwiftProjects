bdee602f8ab8e49192b4a668521da5aaea3ab422
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-08-08 15:06:44 -0800

    Create VixDataApp.swift

---
+++ YouTube_SwiftTutorials/CoreML_SwiftUI/V4/VixDataApp.swift
@@ -0,0 +1,21 @@
+//
+// VixData
+// VixDataApp.swift
+//
+// Created on 8/5/24
+// Swift Version 5.0
+//
+// ATS Project
+//
+
+
+import SwiftUI
+
+@main
+struct VixDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
