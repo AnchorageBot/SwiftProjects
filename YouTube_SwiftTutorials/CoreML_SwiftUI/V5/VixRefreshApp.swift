ef05358a172cc04c412f3337eaa9e975b36a9268
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-08-11 11:09:21 -0800

    Create VixRefreshApp.swift

---
+++ YouTube_SwiftTutorials/CoreML_SwiftUI/V5/VixRefreshApp.swift
@@ -0,0 +1,21 @@
+//
+// VixRefresh
+// VixRefreshApp.swift
+//
+// Created on 8/11/24
+// Swift Version 5.0
+//
+// ATS Project
+//
+
+
+import SwiftUI
+
+@main
+struct VixRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
