78624fc8134e357a13714e652f49e461c1d7c9fd
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-08-17 09:12:57 -0800

    Create BetterRestApp.swift

---
+++ YouTube_SwiftTutorials/CoreML_SwiftUI/V7/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Paul Hudson on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
