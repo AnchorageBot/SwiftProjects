57aefe524bd7798035f7db50e2abe27dea09a8d4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-05 19:37:52 -0800

    Create TipCalculatorApp.swift

---
+++ YouTube_SwiftTutorials/UnitTesting/V1/TipCalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+// TipCalculator
+// TipCalculatorApp.swift
+//
+// Created on 9/5/24
+// Swift Version 5.0
+//
+// ATS Project
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
