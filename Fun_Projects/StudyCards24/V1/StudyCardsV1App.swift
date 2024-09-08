6009791081b05b2fc03050b25aaeae84425553bb
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-08 09:13:03 -0800

    Create StudyCardsV1App.swift

---
+++ Fun_Projects/StudyCards24/V1/StudyCardsV1App.swift
@@ -0,0 +1,22 @@
+//
+// StudyCardsV1
+// StudyCardsV1App.swift
+//
+// Created on 9/8/24
+// Swift Version 5.0
+//
+// ATS Project
+// Tutor Anthropic's AI Claude
+//
+
+
+import SwiftUI
+
+@main
+struct StudyCardsV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
