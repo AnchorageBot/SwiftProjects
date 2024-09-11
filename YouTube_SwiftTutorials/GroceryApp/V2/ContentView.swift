e9d46d129880c0bfdae925e160494e34bd10a343
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-10 20:02:38 -0800

    Create ContentView.swift

---
+++ YouTube_SwiftTutorials/GroceryApp/V2/ContentView.swift
@@ -0,0 +1,89 @@
+//
+// GroceryList
+// ContentView.swift
+//
+// Created on 9/10/24
+// Swift Version 5.0
+//
+// ATS Project
+//
+
+/*
+ 
+ Abstract:
+ 
+ Creates a grocery list
+ 
+ App strategy
+ * Model: Data
+ * Container: Where things are saved
+ * Context: How user interacts with the app
+ 
+ */
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    
+    @Environment(\.modelContext) var context
+    
+    @State var newItemString = ""
+    
+    @Query var items: [GroceryListItem]
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            VStack {
+                TextField("Add item", text: $newItemString)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button("Save") {
+                    guard !newItemString.isEmpty else {
+                        return
+                    }
+                    
+                    let newItem = GroceryListItem(title: newItemString, subtitle: "Buy this ASAP", date: Date())
+                    
+                    context.insert(newItem)
+                    
+                    newItemString = ""
+                    
+                }
+                
+                    
+                List {
+                    ForEach(items) { item in
+                        Text(item.title)
+                    }
+//                        VStack {
+//                            Text(item.title)
+//                            Text(item.subtitle)
+//                            Text(item.date.formatted())
+//                        }
+                        .onDelete {
+                            indexSet in indexSet.forEach({ index in context.delete(items[index])
+                            })
+                    }
+                }
+                    
+                .overlay {
+                    if items.isEmpty {
+                        Text("No items")
+                        }
+                    }
+                }
+            
+            .navigationTitle("Grocery List")
+            }
+            
+            //.padding()
+        }
+    }
+
+
+#Preview {
+    ContentView()
+}
