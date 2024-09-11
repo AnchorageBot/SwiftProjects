a4b73fe437453920ea446a6d21e0d00369ce3bf3
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-10 20:02:15 -0800

    Create GroceryListApp.swift

---
+++ YouTube_SwiftTutorials/GroceryApp/V2/GroceryListApp.swift
@@ -0,0 +1,25 @@
+//
+// GroceryList
+// GroceryListApp.swift
+//
+// Created on 9/10/24
+// Swift Version 5.0
+//
+// ATS Project
+//
+
+
+import SwiftUI
+import SwiftData
+
+@main
+
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        /// inject a container to hold things
+        .modelContainer(for: GroceryListItem.self)
+    }
+}
