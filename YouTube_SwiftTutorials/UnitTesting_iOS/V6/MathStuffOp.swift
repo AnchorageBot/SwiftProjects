784807cac7d0966a76d2dd92861fb999222fe81c
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-24 18:29:24 -0800

    Create MathStuffOp.swift

---
+++ YouTube_SwiftTutorials/UnitTesting_iOS/V6/MathStuffOp.swift
@@ -0,0 +1,26 @@
+//
+// MathStuffV1
+// MathStuffOp.swift
+//
+// Created on 9/23/24
+// Revised on 9/24/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import Foundation
+
+public class MathStuffOperations {
+    
+    func addNumbers(_ a: Int, _ b: Int) -> Int {
+        return a + b
+        }
+        
+    func subtractNumbers(_ a: Int, _ b: Int) -> Int {
+            return a - b
+        }
+        
+}
