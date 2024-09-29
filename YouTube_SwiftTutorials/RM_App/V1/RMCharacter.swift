08d3564b0a0625860b3e3215cf422637f509b302
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-28 21:52:29 -0800

    Create RMCharacter.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1/RMCharacter.swift
@@ -0,0 +1,13 @@
+//
+// RickAndMorty
+// RMCharacter.swift
+//
+// Created on 9/28/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import Foundation
