c586a008a778bc48ac5002f4b446a1defdbd3cef
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-28 21:50:40 -0800

    Create RMEpisodeViewController.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1/RMEpisodeViewController.swift
@@ -0,0 +1,23 @@
+//
+// RickAndMorty
+// RMEpisodeViewController.swift
+//
+// Created on 9/28/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import UIKit
+
+final class RMEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
