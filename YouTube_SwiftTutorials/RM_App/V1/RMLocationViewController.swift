2fac2a1e6312eb1629a9d9cbe6aa2cebab4e266e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-28 21:51:09 -0800

    Create RMLocationViewController.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1/RMLocationViewController.swift
@@ -0,0 +1,23 @@
+//
+// RickAndMorty
+// RMLocationViewController.swift
+//
+// Created on 9/28/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import UIKit
+
+final class RMLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
