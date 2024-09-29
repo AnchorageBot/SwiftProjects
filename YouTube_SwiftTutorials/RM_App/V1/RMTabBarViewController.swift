9f430663d31c8ad5cd684f66277e7fe674d628b7
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-09-28 21:52:03 -0800

    Create RMTabBarViewController.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1/RMTabBarViewController.swift
@@ -0,0 +1,24 @@
+//
+// RickAndMorty
+// RMTabBarViewController.swift
+//
+// Created on 9/28/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import UIKit
+
+final class RMTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
