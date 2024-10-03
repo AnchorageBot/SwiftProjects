414d131623cbf50e92a213facdca387016d0891a
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-02 18:58:14 -0800

    Update RMTabBarViewController.swift

--- YouTube_SwiftTutorials/RM_App/V1/RMTabBarViewController.swift
+++ YouTube_SwiftTutorials/RM_App/V1/RMTabBarViewController.swift
@@ -3,6 +3,7 @@
 // RMTabBarViewController.swift
 //
 // Created on 9/28/24
+// Updated on 10/2/24
 // Swift Version 6.0
 //
 // ATS Project
@@ -17,8 +18,27 @@ final class RMTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        setupTabs()
     }
 
-
+    private func setupTabs() {
+        let characterVC = RMCharacterViewController()
+        let episodeVC = RMEpisodeViewController()
+        let locationVC = RMLocationViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        characterVC.title = "Characters"
+        episodeVC.title = "Episodes"
+        locationVC.title = "Locations"
+        settingsVC.title = "Settings"
+        
+        let nav1 = UINavigationController(rootViewController: characterVC)
+        let nav2 = UINavigationController(rootViewController: episodeVC)
+        let nav3 = UINavigationController(rootViewController: locationVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+    }
+    
 }
 
