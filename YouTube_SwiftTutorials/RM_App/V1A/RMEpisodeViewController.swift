6c7f20e818c65e2298fff8cfa0d1f054b5ba4b86
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-13 10:36:41 -0800

    Create RMEpisodeViewController.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1A/RMEpisodeViewController.swift
@@ -0,0 +1,25 @@
+//
+// RickAndMorty
+// RMEpisodeViewController.swift
+//
+// Created on 9/28/24
+// Updated on 10/13/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import UIKit
+
+final class RMEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Supports changes to background color, light mode or dark mode
+        view.backgroundColor = .systemBackground
+        title = "Episodes"
+    }
+    
+}
