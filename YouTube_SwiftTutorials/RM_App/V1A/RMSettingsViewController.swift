775f998bb0ab21469b3c2b1cd77a2cec678c85f4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-13 10:37:53 -0800

    Create RMSettingsViewController.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1A/RMSettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+// RickAndMorty
+// RMSettingsViewController.swift
+//
+// Created on 9/28/24
+// Updated on 10/13/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import UIKit
+
+final class RMSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Supports changes to background color, light mode or dark mode
+        view.backgroundColor = .systemBackground
+        title = "Settings"
+    }
+    
+}
