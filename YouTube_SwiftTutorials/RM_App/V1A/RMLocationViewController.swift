20e62f122c894a93bd7b0ce52c1beae3703d83ea
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-13 10:37:24 -0800

    Create RMLocationViewController.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1A/RMLocationViewController.swift
@@ -0,0 +1,25 @@
+//
+// RickAndMorty
+// RMLocationViewController.swift
+//
+// Created on 9/28/24
+// Updated on 10/13/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import UIKit
+
+final class RMLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Supports changes to background color, light mode or dark mode
+        view.backgroundColor = .systemBackground
+        title = "Locations"
+    }
+    
+}
