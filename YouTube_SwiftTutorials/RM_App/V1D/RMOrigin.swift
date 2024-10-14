70af1fae5f4917b5b98678935f56fe3a696cafd7
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-14 12:08:05 -0800

    Create RMOrigin.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1D/RMOrigin.swift
@@ -0,0 +1,18 @@
+//
+// RickAndMorty
+// RMOrigin.swift
+//
+// Created on 10/13/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
