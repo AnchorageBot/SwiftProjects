799d5fff00ec8da904e43727b9bc554e8f1bfa23
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-14 12:06:01 -0800

    Create RMCharacterGender.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1D/RMCharacterGender.swift
@@ -0,0 +1,20 @@
+//
+// RickAndMorty
+// RMCharacterGender.swift
+//
+// Created on 10/14/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case unknown = ""
+}
