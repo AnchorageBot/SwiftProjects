91aa80eb141f537f16d5407e1506d5c8f5a2b2e6
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-13 21:33:44 -0800

    Create RMLocation.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1C/RMLocation.swift
@@ -0,0 +1,18 @@
+//
+// RickAndMorty
+// RMLocation.swift
+//
+// Created on 9/28/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import Foundation
+
+struct RMLocation: Codable {
+    let name: String
+    let url: String
+}
