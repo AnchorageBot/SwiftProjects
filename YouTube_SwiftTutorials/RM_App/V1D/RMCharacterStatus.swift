7e8f397a6af2efbd6349c1a1871735f4ce1223b6
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-14 12:06:27 -0800

    Create RMCharacterStatus.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1D/RMCharacterStatus.swift
@@ -0,0 +1,19 @@
+//
+// RickAndMorty
+// RMCharacterStatus.swift
+//
+// Created on 10/14/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = ""
+}
