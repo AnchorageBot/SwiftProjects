7439c25ec410004c1ea490ace5a2b61111543e13
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-10-13 16:09:25 -0800

    Create RMRequest.swift

---
+++ YouTube_SwiftTutorials/RM_App/V1B/RMRequest.swift
@@ -0,0 +1,18 @@
+//
+// RickAndMorty
+// RMRequest.swift
+//
+// Created on 10/13/24
+// Swift Version 6.0
+//
+// ATS Project
+//   Tutor: Afraz Siddiqui - iOS Academy YouTube
+//
+
+
+import Foundation
+
+// Object that represents a singlet API call
+final class RMRequest {
+    
+}
