d620187bcd91037200f7a8b9fcd4eb18c166d7da
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-12-12 23:01:35 -0900

    Create BayesCalculatorTests.swift

---
+++ Fun_Projects/Bayesian/WHBS/V5/BayesCalculatorTests.swift
@@ -0,0 +1,21 @@
+//
+// BayesCalculator
+// BayesCalculatorTests.swift
+//
+// Created on 12/12/24
+// Swift Version 6.0
+//
+// ATS Project
+//
+
+
+import Testing
+@testable import BayesCalculator
+
+struct BayesCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
