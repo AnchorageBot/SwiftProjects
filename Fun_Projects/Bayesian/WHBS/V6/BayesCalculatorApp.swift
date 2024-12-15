fccec2b57f5b6993e229317c215a648de4a2575d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-12-14 23:38:41 -0700

    Create BayesCalculatorApp.swift

---
+++ Fun_Projects/Bayesian/WHBS/V6/BayesCalculatorApp.swift
@@ -0,0 +1,21 @@
+//
+// BayesCalculator
+// BayesCalculatorApp.swift
+//
+// Created on 12/12/24
+// Swift Version 6.0
+//
+// ATS Project
+// Tutor: Anthropic's AI Claude
+//
+
+import SwiftUI
+
+@main
+struct BayesCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
