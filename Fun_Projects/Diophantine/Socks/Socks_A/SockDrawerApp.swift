7b905f40ce0f59085f0b649ce4b60225b16b92e6
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2024-12-26 17:49:40 -0800

    Create SockDrawerApp.swift

---
+++ Fun_Projects/Diophantine/Socks/Socks_A/SockDrawerApp.swift
@@ -0,0 +1,22 @@
+//
+// SockDrawer
+// SockDrawerApp.swift
+//
+// Created on 12/26/24
+// Swift Version 6.0
+//
+// ATS Project
+// Tutor: Google Gemini 2.0
+//
+
+
+import SwiftUI
+
+@main
+struct SockDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // Use ContentView here
+        }
+    }
+}
