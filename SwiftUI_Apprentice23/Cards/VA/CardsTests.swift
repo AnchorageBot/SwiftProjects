7fa1ce474d1d34a88a877130495aa6408f7a8675
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2025-01-01 10:42:08 -0800

    Create CardsTests.swift

---
+++ SwiftUI_Apprentice23/Cards/VA/CardsTests.swift
@@ -0,0 +1,24 @@
+//
+// Cards
+// CardsTests.swift
+//
+// Created on 1/1/25
+// Swift Version 6.0
+//
+// ATS Project
+// Kodeco Swift UI Apprentice 23 Tutorial
+//
+// Abstract:
+// This application implements ...
+
+
+import Testing
+@testable import Cards
+
+struct CardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
