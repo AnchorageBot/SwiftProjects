9d95e851159ee467d0e1d698651056f5b8beb6a3
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2025-01-01 19:48:25 -0800

    Create SingleCardView.swift

---
+++ SwiftUI_Apprentice23/Cards/VB/SingleCardView.swift
@@ -0,0 +1,26 @@
+//
+// Cards
+// SingleCardView.swift
+//
+// Created on 1/1/25
+// Swift Version 6.0
+//
+// ATS Project
+// Kodeco Swift UI Apprentice 23 Tutorial
+//
+// Abstract:
+// This application implements ...
+
+
+import SwiftUI
+
+struct SingleCardView: View {
+    
+    var body: some View {
+      Color.yellow
+    }
+}
+
+#Preview {
+    SingleCardView()
+}
