3ecc6e184830a2031275acee396b48d6e23df29a
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2025-01-08 19:19:04 -0900

    Create ToolbarSelection.swift

---
+++ SwiftUI_Apprentice23/Cards/VF/ToolbarSelection.swift
@@ -0,0 +1,18 @@
+//
+// Cards
+// ToolbarSelection.swift
+//
+// Created on 1/1/25
+// Swift Version 6.0
+//
+// ATS Project
+// Kodeco Swift UI Apprentice 23 Tutorial
+// Tutor: Google's Gemini 2.0 Experimental
+//
+// Abstract:  This SwiftUI application serves as a foundation for a card-based information display system. It currently implements a scrollable list of card thumbnails with full-screen detail views. Future development will include features such as [mention planned features, e.g., data loading from a remote source, user interaction with card details, etc.]
+
+import Foundation
+
+enum ToolbarSelection {
+  case photoModal, frameModal, stickerModal, textModal
+}
