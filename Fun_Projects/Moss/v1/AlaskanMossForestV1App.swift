8368d6f7986de23ec95be27436bf2acd022c19a8
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2025-03-29 12:54:07 -0800

    Create AlaskanMossForestV1App.swift

---
+++ Fun_Projects/Moss/v1/AlaskanMossForestV1App.swift
@@ -0,0 +1,22 @@
+//
+// AlaskanMossForestV1
+// AlaskanMossForestV1App.swift
+//
+// Created on 10/27/24
+// Swift Version 6.0
+//
+// ATS Project
+// Tutor: Anthropic's AI, Claude
+//
+
+
+import SwiftUI
+
+@main
+struct AlaskanMossForestV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
