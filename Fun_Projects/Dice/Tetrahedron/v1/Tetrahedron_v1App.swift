97c4b0690cc386223a2242fddc0b275ab499ff51
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2025-05-01 19:41:43 -0800

    Create Tetrahedron_v1App.swift

---
+++ Fun_Projects/Dice/Tetrahedron/v1/Tetrahedron_v1App.swift
@@ -0,0 +1,24 @@
+//
+// Tetrahedron_v1
+// Tetrahedron_v1App.swift
+//
+// Created on 5/1/25
+// Swift Version 6.0
+//
+// ATS Project
+// Assisted by Anthropic's AI Claude
+//
+
+
+// This is the entry point for the app
+
+import SwiftUI
+
+@main
+struct Tetrahedron_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
