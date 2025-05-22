709730e3bbd7bf1bd1d9ea496b96b525337afc99
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2025-05-21 22:13:40 -0800

    Create ETF_v2App.swift

---
+++ Fun_Projects/Etf/v2/ETF_v2App.swift
@@ -0,0 +1,21 @@
+//
+// ETF_v2
+// ETF_v2App.swift
+//
+// Created on 5/21/25
+// Swift Version 6.0
+//
+// ATS Project
+// Assisted by Google Gemini Advanced 2.5 Pro
+//
+
+import SwiftUI
+
+@main
+struct ETF_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
